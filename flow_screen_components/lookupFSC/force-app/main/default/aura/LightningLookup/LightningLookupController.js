cc4b5764152a25fcb4b095239b23387ce884344e
Eric Smith <ericrsmith35@gmail.com>
2018-07-05 15:26:31 -0400

    Update LightningLookupController.js

--- flow_screen_components/lookupFSC/force-app/main/default/aura/LightningLookup/LightningLookupController.js
+++ flow_screen_components/lookupFSC/force-app/main/default/aura/LightningLookup/LightningLookupController.js
@@ -14,76 +14,85 @@ LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRA
 IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 ({
-	doInit : function(component, event, helper) {
+    doInit : function(component, event, helper) {
+        
+        // If a default value is specified, override the whereClause to select it
+        component.set("v.saveWhereClause", component.get('v.whereClause'));
+        var defaultV = component.get('v.defaultValue');   
+        if(!(!defaultV || defaultV == '')){
+            var newWhere = component.get('v.displayedFieldName')+"='"+defaultV+"'"
+            component.set("v.whereClause", newWhere);
+        }
+        
         helper.hlpGetFieldHelp(component);
-		helper.hlpGetField(component);
-		
-	},
-	performLookup : function(component, event, helper) {
-		helper.hlpPerformLookup(component);
-	},
-	selectItem : function(component, event, helper) {
-		var index = event.currentTarget.dataset.index;
-		helper.hlpSelectItem(component, index);
-	},
-	toggleMenu : function(component, event, helper) {
-		helper.hlpToggleMenu(component);
-	},
-	checkValidity : function(component, event, helper) {
-		helper.hlpCheckValidity(component);
-	},
-	setHelpTextProperties : function(component, event, helper) {
-		helper.hlpSetHelpTextProperties(component);
-	},
-	setInputValue : function(component, event, helper) {
-		var selectedName = component.get("v.selectedName");
-		document.getElementById(component.getGlobalId() + "_myinput").value = selectedName;
+        helper.hlpGetField(component);
+
+    },
+    performLookup : function(component, event, helper) {
+        helper.hlpPerformLookup(component);
+    },
+    selectItem : function(component, event, helper) {
+        var index = event.currentTarget.dataset.index;
+        helper.hlpSelectItem(component, index);
+    },
+    toggleMenu : function(component, event, helper) {
+        helper.hlpToggleMenu(component);
+    },
+    checkValidity : function(component, event, helper) {
+        helper.hlpCheckValidity(component);
+    },
+    setHelpTextProperties : function(component, event, helper) {
+        helper.hlpSetHelpTextProperties(component);
+    },
+    setInputValue : function(component, event, helper) {
+        var selectedName = component.get("v.selectedName");
+        document.getElementById(component.getGlobalId() + "_myinput").value = selectedName;            
     },
     setFilterInputName : function(component, event, helper) {
-		var selectedFilterName = component.get("v.selectedFilterName");
-		document.getElementById(component.getGlobalId() + "_myinput").value = selectedFilterName;
+        var selectedFilterName = component.get("v.selectedFilterName");
+        document.getElementById(component.getGlobalId() + "_myinput").value = selectedFilterName;
     },
     setFilterInputValue : function(component, event, helper) {
-		var selectedFilterValue = component.get("v.selectedFilterValue");
-		document.getElementById(component.getGlobalId() + "_myinput").value = selectedFilterValue;
-	},
-	getMasterFilterValue : function(component, event, helper) {
-		if(component.get('v.parentChild') == 'Child'){
-			component.set("v.masterFilterValue", event.getParam("MasterFilterValue"));
-			helper.clearField(component,true);
-			helper.toggleIcons(component,true);
-		}
-	},
+        var selectedFilterValue = component.get("v.selectedFilterValue");
+        document.getElementById(component.getGlobalId() + "_myinput").value = selectedFilterValue;
+    },
+    getMasterFilterValue : function(component, event, helper) {
+        if(component.get('v.parentChild') == 'Child'){
+            component.set("v.masterFilterValue", event.getParam("MasterFilterValue"));
+            helper.clearField(component,true);
+            helper.toggleIcons(component,true);
+        }
+    },
     clearField : function(component, event, helper){
-		helper.clearField(component,true);
-		helper.toggleIcons(component,true);
-	},
-	/**
+        helper.clearField(component,true);
+        helper.toggleIcons(component,true);
+    },
+    /**
 	 * support for highlighting suggestions using the up and down arrow
 	 * support for selecting highlighted record by pressing Enter
 	 */
-	highlight : function(component, event, helper){
-		var el = $('#lookup-65 ul li');
-		var highlighted = $('#lookup-65 ul li.hlight');
-		if(event.code == 'ArrowDown'){
-			highlighted.removeClass('hlight').next().addClass('hlight');
-			if(highlighted.next().length == 0){
-				el.eq(0).addClass('hlight');
-			}
-		}
-		else if(event.code == 'ArrowUp'){
-			highlighted.removeClass('hlight').prev().addClass('hlight');
-			if(highlighted.prev().length == 0){
-				el.eq(-1).addClass('hlight')
-			}
-		}
-		else if(event.code == 'Enter'){
-			if($A.util.hasClass(component.find("dropDown"),'slds-is-open')){
-				highlighted.click();
-			}
-		}
-	},
-	hover : function(component, event, helper){
-		$('#lookup-65 ul li').removeClass('hlight')
-	}
+    highlight : function(component, event, helper){
+        var el = $('#lookup-65 ul li');
+        var highlighted = $('#lookup-65 ul li.hlight');
+        if(event.code == 'ArrowDown'){
+            highlighted.removeClass('hlight').next().addClass('hlight');
+            if(highlighted.next().length == 0){
+                el.eq(0).addClass('hlight');
+            }
+        }
+        else if(event.code == 'ArrowUp'){
+            highlighted.removeClass('hlight').prev().addClass('hlight');
+            if(highlighted.prev().length == 0){
+                el.eq(-1).addClass('hlight')
+            }
+        }
+            else if(event.code == 'Enter'){
+                if($A.util.hasClass(component.find("dropDown"),'slds-is-open')){
+                    highlighted.click();
+                }
+            }
+    },
+    hover : function(component, event, helper){
+        $('#lookup-65 ul li').removeClass('hlight')
+    }
 })

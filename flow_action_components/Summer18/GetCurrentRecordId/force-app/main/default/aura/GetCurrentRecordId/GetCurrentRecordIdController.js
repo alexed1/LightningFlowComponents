bf1431bc3d7de231cd7b531f50605bb1c26190b2
Alex Edelstein <aedelstein@salesforce.comm>
2018-07-21 16:43:41 -0700

    cleanup Get Current RecordId

--- flow_action_components/Summer18/GetCurrentRecordId/force-app/main/default/aura/GetCurrentRecordId/GetCurrentRecordIdController.js
+++ flow_action_components/Summer18/GetCurrentRecordId/force-app/main/default/aura/GetCurrentRecordId/GetCurrentRecordIdController.js
@@ -1,17 +1,11 @@
 ({
   invoke : function(component, event, helper) {
-	var args = event.getParam("arguments");
-		 
 
-	var currentUrlString = window.location.href;
-	console.log("currentURL is: "+ currentUrlString);
-
-    	var urlSegments = currentUrlString.split('/');
-    	var recordid = urlSegments[urlSegments.length - 2];
-	console.log("recordid is " + recordid);
+		var currentUrlString = window.location.href;
+    var urlSegments = currentUrlString.split('/');
+    var recordid = urlSegments[urlSegments.length - 2];
     	
-      	component.set("v.recordId", recordid);
-	       
+    component.set("v.recordId", recordid); 
 
    }
 })

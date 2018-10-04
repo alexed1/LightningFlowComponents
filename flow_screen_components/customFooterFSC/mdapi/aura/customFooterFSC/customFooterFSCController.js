82566aa8284faa33b2add497cd45811d0634d21f
Alex Edelstein <aedelstein@salesforce.comm>
2018-10-03 22:00:50 -0400

    customFooter and horizontalRule

---
+++ flow_screen_components/customFooterFSC/mdapi/aura/customFooterFSC/customFooterFSCController.js
@@ -0,0 +1,23 @@
+({
+	handleClick : function(component, event, helper) {
+        
+        var buttonName = event.getSource().get("v.name");
+		var action ="";
+        
+        if (buttonName == 'ButtonNext')
+            if (component.get("v.onFinalPage") == true)
+                action="FINAL";
+        	else
+            	action="NEXT";
+        else 
+            action="BACK";
+        console.log("action is: "+ action);
+
+  	
+
+        var navigate = component.get('v.navigateFlow');  
+        navigate(action); 
+        
+		
+    }
+})

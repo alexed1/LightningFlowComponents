c0d8a0c4976c8ec06175d7f6815da1a31e87f966
Alex Edelstein <aedelstein@salesforce.comm>
2018-11-22 12:25:25 -0800

    improvements to navigationButton

--- flow_screen_components/navigationButtonFSC/force-app/main/default/aura/navigationButtonFSC/navigationButtonFSCController.js
+++ flow_screen_components/navigationButtonFSC/force-app/main/default/aura/navigationButtonFSC/navigationButtonFSCController.js
@@ -1,27 +1,10 @@
 ({
     handleClick : function(component, event, helper) {
-        var destUrl = component.get("v.destinationURL");
-        var navigationGoal = component.get("v.navigationType");
-        
-        component.set("v.fire", true);
-
-        var navigate = component.get('v.navigateFlow');
-        
-        if (navigationGoal == 'finish') {
-          navigate("FINISH");  
-        }
-        
-		if (navigationGoal == 'next') {
-          navigate("NEXT");  
-        }  
-        if (navigationGoal == 'back') {
-            navigate("BACK");  
-        }  
-    },
-    
-	navigateout: function (component, event) {
+        		console.log('entering handleclick');
+        		component.set("v.fire", true);
               	var destType = component.get("v.destinationType");
-              	if (destType == 'url') { 
+              	if (destType == 'url') {
+                    console.log('destType is url');
                     var urlEvent = $A.get("e.force:navigateToURL");
              		console.log('destination is url');
     				var destUrl = component.get("v.destinationURL");
@@ -45,26 +28,47 @@
 					}
                 }
             	else {
-                   	if (destType == 'record') {
+                    if (destType == 'record') {
                         var urlEvent = $A.get("e.force:navigateToSObject");
-                       	console.log('destination is record');
-           				var destObject = component.get("v.targetRecordId");
-           				console.log('recordId is:' +destObject)
-           				urlEvent.setParams({
-             				"recordId": destObject,
-              				"slideDevName": "related",
-            				"isredirect": true
-                	    });
-            			console.log("running urlEvent for Record ID")
-          				if( (typeof sforce != 'undefined') && (sforce.one != null) ) {
-							//handle lightning experience on desktop
-							console.log("running navigation for lightning desktop");
+                        console.log('destination is record');
+                        var destObject = component.get("v.targetRecordId");
+                        console.log('recordId is:' +destObject)
+                        urlEvent.setParams({
+                            "recordId": destObject,
+                            "slideDevName": "related",
+                            "isredirect": true
+                        });
+                        console.log("running urlEvent for Record ID")
+                        if( (typeof sforce != 'undefined') && (sforce.one != null) ) {
+                            //handle lightning experience on desktop
+                            console.log("running navigation for lightning desktop");
                             //appears to be broken
                             urlEvent.fire();
-                        	}
+                        }
                         else {
                             console.log("running navigation for classic")
                             window.location = '/'+destObject; 
+                        }
+                    }
+                    
+                    else {
+                        if (destType == 'standard') {
+                            console.log('entering standard');
+                            var navigationGoal = component.get("v.navigationType");
+        
+                            //component.set("v.fire", true);
+                            var navigate = component.get('v.navigateFlow');
+                            
+                            if (navigationGoal.toLowerCase() == 'finish') {
+                              navigate("FINISH");  
+                            }
+                            
+                            if (navigationGoal.toLowerCase() == 'next') {
+                              navigate("NEXT");  
+                            }  
+                            if (navigationGoal.toLowerCase() == 'back') {
+                              navigate("BACK");  
+                            }  
                         }
                     }
             	}
--- flow_screen_components/navigationButtonFSC/mdapi/aura/navigationButtonFSC/navigationButtonFSCController.js
+++ flow_screen_components/navigationButtonFSC/mdapi/aura/navigationButtonFSC/navigationButtonFSCController.js
@@ -1,11 +1,72 @@
 ({
     handleClick : function(component, event, helper) {
-        var destUrl = component.get("v.destinationURl");
+        var destUrl = component.get("v.destinationURL");
+        var navigationGoal = component.get("v.navigationType");
         
-        var urlEvent = $A.get("e.force:navigateToURL");
-        urlEvent.setParams({
-          "url": destUrl
-        });
-        urlEvent.fire();
-    }
+        component.set("v.fire", true);
+
+        var navigate = component.get('v.navigateFlow');
+        
+        if (navigationGoal == 'finish') {
+          navigate("FINISH");  
+        }
+        
+		if (navigationGoal == 'next') {
+          navigate("NEXT");  
+        }  
+        if (navigationGoal == 'back') {
+            navigate("BACK");  
+        }  
+    },
+    
+	navigateout: function (component, event) {
+              	var destType = component.get("v.destinationType");
+              	if (destType == 'url') { 
+                    var urlEvent = $A.get("e.force:navigateToURL");
+             		console.log('destination is url');
+    				var destUrl = component.get("v.destinationURL");
+             		console.log('url is '+destUrl)	
+          			if( (typeof sforce != 'undefined') && (sforce.one != null) ) {
+						//handle lightning experience on desktop
+						console.log("running navigation for lightning desktop");
+                		urlEvent.setParams({
+            				"url": destUrl
+        	 			});      
+                        console.log("Firing urlEvent: "+urlEvent)
+                    //appears to be broken
+            		urlEvent.fire();
+        			}
+    				else {
+            			var device = $A.get("$Browser.formFactor");
+            			if (device=="DESKTOP") {
+             		   		console.log("running navigation for classic desktop");
+            		    	window.location = destUrl;
+           				}
+					}
+                }
+            	else {
+                   	if (destType == 'record') {
+                        var urlEvent = $A.get("e.force:navigateToSObject");
+                       	console.log('destination is record');
+           				var destObject = component.get("v.targetRecordId");
+           				console.log('recordId is:' +destObject)
+           				urlEvent.setParams({
+             				"recordId": destObject,
+              				"slideDevName": "related",
+            				"isredirect": true
+                	    });
+            			console.log("running urlEvent for Record ID")
+          				if( (typeof sforce != 'undefined') && (sforce.one != null) ) {
+							//handle lightning experience on desktop
+							console.log("running navigation for lightning desktop");
+                            //appears to be broken
+                            urlEvent.fire();
+                        	}
+                        else {
+                            console.log("running navigation for classic")
+                            window.location = '/'+destObject; 
+                        }
+                    }
+            	}
+    }  
 })

8d4072b4bc8016e27c7c399483e07f299f40a709
Eric Smith <ericrsmith35@gmail.com>
2019-07-01 13:24:33 -0400

    Update lookupFSCHelper.js
    Remove validity checker

--- flow_screen_components/lookupFSC/force-app/main/default/aura/lookupFSC/lookupFSCHelper.js
+++ flow_screen_components/lookupFSC/force-app/main/default/aura/lookupFSC/lookupFSCHelper.js
@@ -1,17 +1,5 @@
 ({
-    hlpCheckValidity : function(component, event){                
-        component.set('v.validate', function() {         
-        var selectedValue = component.get("v.selectedValue");
-        var required = component.get("v.required"); 
-        if(!required || (selectedValue && !$A.util.isEmpty(selectedValue))) { 
-            return { isValid: true }; 
-        } 
-        else {                 
-            return { 
-                isValid: false, 
-                errorMessage: 'Please select a choice.' 
-            }; 
-        }
-        })    
+    helperMethod : function() {
+
     }
 })

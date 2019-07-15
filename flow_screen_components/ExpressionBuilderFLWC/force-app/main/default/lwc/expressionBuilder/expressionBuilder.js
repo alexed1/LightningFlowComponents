0eadbe992e06a0144003bba6c136e74efb0bd0e5
Alex Edelstein <aedelstein@salesforce.comm>
2019-07-15 12:18:55 -0700

    updates expressionbuilder

--- flow_screen_components/ExpressionBuilderFLWC/force-app/main/default/lwc/expressionBuilder/expressionBuilder.js
+++ flow_screen_components/ExpressionBuilderFLWC/force-app/main/default/lwc/expressionBuilder/expressionBuilder.js
@@ -1,11 +1,11 @@
 import { LightningElement, api } from 'lwc';
 
+export default class ExpressionBuilder extends LightningElement {
 
-export default class Multiline extends LightningElement {
-    
-    @api lineItemData;	 
+    @api logicType;
+    @api value;	 
     @api availableLHSObjectFields;
     @api availableRHSMergeFields;
     @api addButtonLabel;
-
+    
 }
--- flow_screen_components/ExpressionBuilderFLWC/force-app/main/default/lwc/expressionLine/expressionLine.js
+++ flow_screen_components/ExpressionBuilderFLWC/force-app/main/default/lwc/expressionLine/expressionLine.js
@@ -1,7 +1,14 @@
 /* eslint-disable no-console */
-import { LightningElement } from 'lwc';
+import { LightningElement, api} from 'lwc';
 
 export default class ExpressionLine extends LightningElement {
+
+    @api logicType;	
+    @api value;	 
+    @api availableLHSObjectFields;
+    @api availableRHSMergeFields;
+    @api addButtonLabel;
+
 
     handleChange(event) {
         console.log('handleChange triggered: ' + event.target.value);

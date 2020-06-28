825c50a0187dce10c361894e3615f27837c29631
NickSauer7 <nms6h@alumni.virginia.edu>
2020-06-27 21:12:28 -0400

    inputRichTextFSC Enhancements
    Updates to input Rich Text.  See ReadMe for details.

--- flow_screen_components/richTextInputFSC/README.md
+++ flow_screen_components/richTextInputFSC/README.md
@@ -1,3 +1,45 @@
 # Rich Text Edit Control #
 
 https://unofficialsf.com/rich-text-input-flow-screen-component/
+
+
+# inputRichTextFSC
+Rich Text enhancements for Flows
+
+### Summary:
+IMPORTANT:  Use richerText = true input attribute if you want to leverage these enhanced features.  Otherwise leave empty and regular input Rich Text component will be used.
+1)	Block disallowed words and symbols
+    a.	Control whether this is a block that prevents next/finish of flow.
+2)	Search and Replace custom text
+3)	Auto-Replace suggested words
+4)	Revert mass changes from search/replace or suggested words
+
+### Admin Instructions:
+1)  Follow Input/Output map below.
+2)  Hard Block will prevent user from advancing the Flow screen if there are disallowedWords or disallowedSymbols used.
+3)  Default for disallowed Words/Symbols if hardBlock is false will be a Warning message only.  Setting hardBlock to true will set them to Error messages.
+
+|Parameter	               |I	   |O	     |Information 
+|--------------------------|-----|-------|-------------------------------------------------------------------------------|
+|**richerText**            |X    |       |  Boolean.  Set to true if you want to use enhanced rich text.  Default if false (regular input component)   |
+|**autoReplaceMap**	           |X	   |       |JSON formatted key:value map.  (example => {"Test": "GreatTest™"} )            |
+|**disallowedSymbols**	       |X	   |       |Comma-separated list of words to block.  Example: /,@,*                        |
+|**disallowedWords**	         |X    |	     |Comma-separated list of words to block.  Example: bad,worse,worst              |
+|**warnOnly**	               |X	   |	     |Boolean.  Set to True if you want to allow Next even where disallowed Symbol or Word remains.  Default is false.   |
+|**richText**	                 |X	   |X	     |Input and output RichText that you’ll be editing                               |
+|**label**                      |X     |X        |Input to provide field-level label if desired                                 |
+
+
+### User Instructions:
+1)  While entering text in the Rich Text Area, if you use words or symbols that are flagged as not allowed, you will receive either a warning message (yellow popup) or an error message (red popup).
+    a.  If the warning message is displayed, this is for notification only, and you will be able to proceed.
+    b.  If the error message is shown, this means you will need to correct the noted error prior to continuing.  If you try to click Next/Finish, a validation message will be  given.
+2)  To Search and Replace text, click on the magnifying glass icon. titled 'Search and Replace'
+    a.  A 'Search Text' field is displayed where you enter the text you want to find.
+    b.  A 'Replace with Text' field is shown where you enter the replacement text to be applied where the 'Search Text' is found.
+    c.  Use the 'Check' icon to commit the search/replace.
+    d.  Use the 'Revert Last Change' button that appears after search/replace to undo the changes just made.
+    e.  If you no longer want to use Search/Replace, you can click on the Magnifying Glass icon again to hide these fields.
+3)  To replace suggested terms set up by your admin, click on the Merge icon titled 'Apply Suggested Terms'.  Note this will only be shown if your admin has configured this.
+    a.  The component will find text to be replaced and overwrite with teh replacement term identified by your admin.  
+    b.  Use the 'Revert Last Change' button that appears after search/replace to undo the changes just made. 
--- flow_screen_components/richTextInputFSC/force-app/main/default/lwc/inputRichTextFSC/inputRichTextFSC.js
+++ flow_screen_components/richTextInputFSC/force-app/main/default/lwc/inputRichTextFSC/inputRichTextFSC.js
@@ -1,9 +1,215 @@
-import { LightningElement, api } from 'lwc';
+import { LightningElement, api, track } from 'lwc';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 
-export default class InputRichTextFSC_LWC extends LightningElement {
+export default class RicherTextFSC extends LightningElement {
+
+    //Input and Output Attributes for Flow
     @api value;
+    @api richerText = false;
+    @api disallowedWordsList;
+    @api disallowedSymbolsList;
+    @api autoReplaceMap;
+    @api warnOnly = false;
     @api label;
     formats = ['font', 'size', 'bold', 'italic', 'underline',
         'strike', 'list', 'indent', 'align', 'link',
         'image', 'clean', 'table', 'header', 'color','background','code','code-block','script','blockquote','direction'];
+
+    //Validation hook to use standard in Flow.  Only enforce if 
+    @api validate(){
+        this.value = this.richText;
+        if(!this.richerText || this.warnOnly || (!this.warnOnly && this.isValidCheck)){
+            return {isValid:true};
+        }else{
+            return {
+                isValid:false,
+                errorMessage: 'Cannot Advance - Invalid Symbols/Words Remaind in Rich Text'
+            };
+        }
+    }
+
+    //Other Variables
+    @track richText; //use separate variable for richerText as value is causing conflict
+    @track disallowedWordsArray = [];
+    @track disallowedWords;
+    @track disallowedWordsMessage;
+    @track disallowedSymbolsArray = [];
+    @track disallowedSymbols;
+    @track disallowedSymbolsMessage;
+    @track searchTerm = '';
+    @track replaceValue = '';
+    @track interimValue = '';
+    @track symbolsNotAllowed;
+    @track wordsNotAllowed;
+    @track oldRichText;
+    @track selectTrue = false;
+    @track allowRevert = false;
+    @track autoReplaceEnabled = false;
+    @track disallowedType = 'error';
+    @track disallowedMode = 'sticky';
+    replaceMap = {};
+    regTerm = '';
+    applyTerm = '';
+    symbolTitle = 'Invalid Symbols';
+    wordTitle = 'Invalid Words';
+
+    //Begin functionality
+
+    //Set initial values on load
+    connectedCallback() {
+        if(this.richerText){
+            this.richText = this.value;
+            if(this.disallowedSymbolsList != undefined){
+                this.disallowedSymbolsMessage = 'Do Not Use the Following Symbols: '+this.disallowedSymbolsList;
+                this.disallowedSymbolsArray = this.disallowedSymbolsList.replace(/\s/g,'').split(',');
+                for(let i=0; i<this.disallowedSymbolsArray.length; i++){
+                    if(i == 0){
+                        if(this.disallowedSymbolsArray.length != 1){
+                            this.disallowedSymbols = '['+ this.disallowedSymbolsArray[i] + '|';
+                        }else{
+                            this.disallowedSymbols = '['+ this.disallowedSymbolsArray[i] + ']';
+                        }
+                    } else if (i == (this.disallowedSymbolsArray.length - 1)){
+                        this.disallowedSymbols = this.disallowedSymbols.concat(this.disallowedSymbolsArray[i] + ']');
+                    } else {
+                        this.disallowedSymbols = this.disallowedSymbols.concat(this.disallowedSymbolsArray[i] + '|');
+                    }
+                }
+            }
+    
+            if(this.disallowedWordsList != undefined){
+                this.disallowedWordsMessage = 'Do Not Use the Following Words: '+this.disallowedWordsList;
+                this.disallowedWordsArray = this.disallowedWordsList.replace(/\s/g,'').split(',');
+                for(let i=0; i<this.disallowedWordsArray.length; i++){
+                    if(i == 0){
+                        if(this.disallowedWordsArray.length != 1){
+                            this.disallowedWords = '('+this.disallowedWordsArray[i] + '|';
+                        }else{
+                            this.disallowedWords = '('+this.disallowedWordsArray[i] + ')\\b';
+                        }
+                    } else if (i == (this.disallowedWordsArray.length - 1)){
+                        this.disallowedWords = this.disallowedWords.concat(this.disallowedWordsArray[i] + ')\\b');
+                    } else {
+                        this.disallowedWords = this.disallowedWords.concat(this.disallowedWordsArray[i] + '|');
+                    }
+                }
+            }
+            if(this.disallowedSymbols != undefined) this.symbolsNotAllowed = new RegExp(this.disallowedSymbols);
+            if(this.disallowedWords != undefined) this.wordsNotAllowed = new RegExp(this.disallowedWords);
+            if(this.autoReplaceMap != undefined){
+                this.replaceMap = JSON.parse(this.autoReplaceMap);
+                this.autoReplaceEnabled = true;
+            } 
+            this.isValidCheck = true;
+            //if warn only is set, then change toast type/mode
+            if(this.warnOnly){
+                this.disallowedType = 'warning';
+                this.disallowedMode = 'dismissable';
+            }
+        }
+    }
+
+    //Handle updates to Rich Text field
+    handleTextChange(event) {
+        if (this.symbolsNotAllowed != undefined || this.wordsNotAllowed != undefined) {
+            this.interimValue = (event.target.value).toLowerCase();
+            this.interimValue = this.interimValue.replace(/(<([^>]+)>)/ig, "");
+
+            if(this.symbolsNotAllowed != undefined){
+                if (this.symbolsNotAllowed.test(this.interimValue)) {
+                    this.isValidCheck = false;
+                    const evt = new ShowToastEvent({
+                        title: this.symbolTitle,
+                        message: this.disallowedSymbolsMessage,
+                        variant: this.disallowedType,
+                        mode: this.disallowedMode
+                    });
+                    this.dispatchEvent(evt);
+                }else{
+                    this.isValidCheck = true;
+                    this.richText = event.target.value;
+                }
+            }
+
+            if(this.wordsNotAllowed != undefined){
+                if (this.wordsNotAllowed.test(this.interimValue)) {
+                    this.isValidCheck = false;
+                    const evt = new ShowToastEvent({
+                        title: this.wordTitle,
+                        message: this.disallowedWordsMessage,
+                        variant: this.disallowedType,
+                        mode: this.disallowedMode
+                    });
+                    this.dispatchEvent(evt);
+                }else{
+                    this.isValidCheck = true;
+                    this.richText = event.target.value;
+                }
+            }
+        } else {
+            this.isValidCheck = true;
+            this.richText = event.target.value;
+        }
+    }
+
+    //Handle initiation of Search and Replace
+    handleOpenSearch(event) {
+        this.selectTrue = !this.selectTrue;
+        if (this.searchButton) this.searchButton = false;
+        else this.searchButton = true;
+    }
+
+    //Search and Replace Search for Value
+    handleSearchChange(event) {
+        this.searchTerm = (event.target.value).replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
+    }
+
+    //Search and Replace Replace with Value
+    handleReplaceChange(event) {
+        this.replaceValue = (event.target.value).replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
+    }
+
+    //Execute Search and REplace
+    searchReplace() {
+        this.oldRichText = this.richText;
+        this.allowRevert = true;
+        let draftValue = this.richText;
+        this.searchTerm = this.escapeRegExp(this.searchTerm);
+        this.replaceValue = this.escapeRegExp(this.replaceValue);
+        draftValue = this.replaceAll(draftValue, this.searchTerm, this.replaceValue);
+        this.isValidCheck = true;
+        this.richText = draftValue;
+    }
+
+    //Execute Auto-Replacement based on map.
+    applySuggested(event) {
+        this.oldRichText = this.richText;
+        this.allowRevert = true;
+        let draftValue = this.richText;
+        let regTerm = '';
+        for (var key in this.replaceMap) {
+            this.applyTerm = this.replaceMap[key];
+            this.regTerm = key;
+            draftValue = this.replaceAll(draftValue, this.regTerm, this.applyTerm);
+        }
+        this.isValidCheck = true;
+        this.richText = draftValue;
+    }
+
+    //Replace All function helper
+    replaceAll(str, term, replacement) {
+        return str.replace(new RegExp(term, 'ig'), replacement);
+    }
+
+    //Undo last change
+    handleRevert() {
+        this.richText = this.oldRichText;
+        this.allowRevert = false;
+    }
+
+    //Clean input for RegExp
+    escapeRegExp(str) {
+        return str.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
+    }
+
 }

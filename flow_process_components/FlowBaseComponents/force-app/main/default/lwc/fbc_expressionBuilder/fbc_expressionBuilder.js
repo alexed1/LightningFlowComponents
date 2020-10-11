7a3851a1e2a9e50ce2b154be7f25f82b2f8354a2
Alex Edelstein <aedelstein@salesforce.comm>
2020-10-10 17:15:14 -0700

    these are primarily changes made to flow base components as part of an effort to turn it into a managed package. found out that a managed version of flow base components would exist in a separate namespace and lwc's can't access separate namespaces for the forseeable future. so this all has to be undone. added a separate branch called fbcchanges in case we need something from this.
    Revert "checkpoint"
    
    This reverts commit ec827c81bc80d7d563d933885340a5c7b81fbbf8.

---
+++ flow_process_components/FlowBaseComponents/force-app/main/default/lwc/fbc_expressionBuilder/fbc_expressionBuilder.js
@@ -0,0 +1,255 @@
+import {LightningElement, track, api, wire} from 'lwc';
+import {FlowAttributeChangeEvent} from 'lightning/flowSupport';
+//import conditionLogicHelpText from '@salesforce/label/c.ConditionLogicHelpText';
+import assembleFormulaString from '@salesforce/apex/fbc_ExpressionBuilder.assembleFormulaString';
+import disassemblyFormulaString from '@salesforce/apex/fbc_ExpressionBuilder.disassemblyFormulaString';
+import describeSObjects from '@salesforce/apex/fbc_SearchUtils.describeSObjects';
+
+export default class fbc_ExpressionBuilder extends LightningElement {
+    @api name;
+    @api addButtonLabel = 'Add Condition';
+    @api dispatchComponentChangeEvents = false;
+    @api customVariableDelimiter;
+    @track objectName;
+    @track expressionLines = [];
+    @track customLogic = '';
+    @track logicType = 'AND';
+    @track convertedExpression;
+    @track contextFields = [];
+    @track contextTypes = [];
+    @track isLoading = true;
+
+    @api customMergeFields = [];
+
+    @api
+    get formulaString() {
+        return this.convertedExpression;
+    }
+
+    set formulaString(value) {
+        this.convertedExpression = value;
+    }
+
+    @api
+    get contextObjectType() {
+        return this._objectName;
+    }
+
+    set contextObjectType(value) {
+        this._objectName = value;
+        if (!this.contextTypes || this.contextTypes.length === 0) {
+            this.contextTypes = [value];
+        }
+    }
+
+    @api //f.e. 'User,Organization,Profile'
+    get supportedSystemTypes() {
+        return this.contextTypes.filter(curObject => curObject !== this._objectName).join(',');
+    }
+
+    set supportedSystemTypes(value) {
+        this.contextTypes = [...[this._objectName], ...this.splitValues(value)];
+    }
+
+    settings = {
+        valueTypeString: 'string',
+        invalidCharactersRegex: /['"\\\/|&^]/g
+    }
+
+    @wire(describeSObjects, {types: '$contextTypes'})
+    _describeSObjects(result) {
+        if (result.error) {
+            console.log(result.error.body);
+        } else if (result.data) {
+            this.contextTypes.forEach(objType => {
+                let newContextFields = result.data[objType].map(curField => {
+                    return {
+                        ...curField, ...{
+                            label: objType + ': ' + curField.label,
+                            value: '$' + curField.type + '.' + curField.value
+                        }
+                    }
+                });
+
+                if (this.contextFields && this.contextFields.length > 0) {
+                    this.contextFields = this.contextFields.concat(newContextFields);
+                } else {
+                    this.contextFields = newContextFields;
+                }
+            });
+
+            if (this.customMergeFields && this.customMergeFields.length) {
+                this.contextFields = this.contextFields.concat(this.customMergeFields);
+            }
+
+            if (this.contextFields && this.contextFields.length > 0) {
+                this.doDisassemblyFormulaString();
+            } else {
+                this.isLoading = false;
+            }
+        }
+    }
+
+    lastExpressionIndex = 0;
+    logicTypes = [
+        {value: 'AND', label: 'All Conditions Are Met'},
+        {value: 'OR', label: 'Any Condition Is Met'}
+        // {value: 'CUSTOM', label: 'Custom Condition Logic Is Met'}
+    ];
+    conditionLogicHelpText = 'placeholder for conditionLogicHelpTest' //conditionLogicHelpText;
+
+    doDisassemblyFormulaString() {
+        disassemblyFormulaString({
+            expression: this.convertedExpression,
+            customVariableDelimiter: this.customVariableDelimiter
+        }).then(result => {
+            if (result.logicType !== undefined) {
+                this.logicType = result.logicType;
+            }
+            if (result.customLogic !== undefined) {
+                this.customLogic = result.customLogic;
+            }
+            if (result.expressionLines !== undefined) {
+                let expressionLines = [];
+                result.expressionLines.forEach((line, index) => {
+                    let fieldData = this.contextFields.find(curField => curField.value === line.fieldName);
+                    expressionLines.push({
+                        ...this.generateNewExpression(), ...{
+                            fieldName: line.fieldName,
+                            id: index,
+                            objectType: line.objectType,
+                            operator: line.operator,
+                            parameter: line.parameter,
+                            renderType: fieldData ? fieldData.renderType : 'text',
+                            dataType: fieldData ? fieldData.dataType : null
+                        }
+                    });
+                    this.lastExpressionIndex = index + 1
+                });
+                this.expressionLines = expressionLines;
+            }
+
+        }).finally(() => {
+            this.isLoading = false;
+        });
+    }
+
+    generateNewExpression() {
+        return {
+            id: this.lastExpressionIndex++,
+            objectType: this.contextObjectType,
+            parameter: ''
+        };
+    }
+
+    handleAddExpression() {
+        this.expressionLines.push(this.generateNewExpression());
+    }
+
+    get showCustomLogicInput() {
+        return this.logicType === 'CUSTOM';
+    }
+
+    handleCustomLogicChange(event) {
+        this.customLogic = event.detail.value;
+        this.assembleFormula();
+    }
+
+    handleWhenToExecuteChange(event) {
+        this.logicType = event.detail.value;
+        this.assembleFormula();
+    }
+
+    handleExpressionChange(event) {
+        let expressionToModify = this.expressionLines.find(curExp => curExp.id === event.detail.id);
+
+        for (let detailKey in event.detail) {
+            if (Object.prototype.hasOwnProperty.call(event.detail, detailKey)) {
+                expressionToModify[detailKey] = event.detail[detailKey];
+            }
+        }
+        if (event.detail.isInit !== true && this.isExpressionValid(expressionToModify)) {
+            this.assembleFormula();
+        }
+    }
+
+    isExpressionValid(expression) {
+        let isParameterValid = true;
+        if (expression.renderType === this.settings.valueTypeString) {
+            isParameterValid = !expression.parameter || !expression.parameter.match(this.settings.invalidCharactersRegex)
+        }
+        return !!(expression.fieldName && expression.operator && expression.parameter && isParameterValid);
+    }
+
+    handleRemoveExpression(event) {
+        this.expressionLines = this.expressionLines.filter(curExp => curExp.id !== event.detail);
+        this.assembleFormula();
+    }
+
+    assembleFormula() {
+        if ((this.logicType === 'CUSTOM' && this.customLogic.length > 0) || this.logicType !== 'CUSTOM') {
+            assembleFormulaString({
+                customLogic: this.customLogic.toUpperCase(),
+                logicType: this.logicType,
+                expressionLines: JSON.stringify(this.expressionLines)
+            }).then(result => {
+                this.convertedExpression = result
+                if (this.dispatchComponentChangeEvents) {
+                    this.dispatchValueChangedEvent();
+                } else {
+                    this.dispatchFlowValueChangedEvent();
+                }
+            })
+        } else {
+            this.convertedExpression = ''
+            this.dispatchFlowValueChangedEvent();
+        }
+    }
+
+    dispatchValueChangedEvent() {
+        const memberRefreshedEvt = new CustomEvent('expressionchanged', {
+            bubbles: true,
+            detail: {
+                name: this.name,
+                value: this.convertedExpression
+            }
+        });
+        this.dispatchEvent(memberRefreshedEvt);
+    }
+
+    dispatchFlowValueChangedEvent() {
+        const valueChangeEvent = new FlowAttributeChangeEvent('value', this.convertedExpression);
+        this.dispatchEvent(valueChangeEvent);
+    }
+
+    get disabledAddButton() {
+        return this.expressionLines.length > 9;
+    }
+
+    splitValues(originalString) {
+        return originalString ? originalString.replace(/ /g, '').split(',') : [];
+    }
+
+    @api
+    getFormulaString() {
+        return this.convertedExpression;
+    }
+
+    @api
+    validate() {
+        let validity = {
+            isValid: true
+        };
+        let inputsToVerify = this.template.querySelectorAll('c-fbc-expression-line');
+        if (inputsToVerify && inputsToVerify.length) {
+            inputsToVerify.forEach(curInput => {
+                let reportedValidity = curInput.validate();
+                if (!reportedValidity || !reportedValidity.isValid) {
+                    validity = reportedValidity;
+                    return reportedValidity;
+                }
+            })
+        }
+        return validity;
+    }
+}
---
+++ flow_process_components/FlowBaseComponents/force-app/main/default/lwc/fbc_transferMetadata/fbc_transferMetadata.js
@@ -0,0 +1,215 @@
+/**
+ * 09/03/20     Eric Smith      Unescaped {} characters in the Metadata String in case it was passed in that way from a Flow
+ * 
+ * 08/21/20     Eric Smith      Added Extracted and Escaped Metadata Strings to the Output Attributes
+ * 
+ * 08/08/20     Eric Smith      Display Error Message on Deploy if one is caught
+ * 
+ * 07/01/20     Eric Smith      Updated to navigate to the next node in the Flow on deployment completion
+ *                              This will work best if you disable the footer on the Flow screen 
+ *                         
+**/
+
+import { LightningElement,track,api,wire } from 'lwc';
+import requestMetadata from '@salesforce/apex/fbc_RetrieveMetadata.fbc_RetrieveMetadataItem';
+import checkRetrieveStatus from '@salesforce/apex/fbc_RetrieveMetadata.checkAsyncRequest';
+import getFileNames from '@salesforce/apex/fbc_RetrieveMetadata.getFileNames';
+import fbc_deployMetadata from '@salesforce/apex/fbc_DeployMetadata.deploy';
+import checkDeployStatus from '@salesforce/apex/fbc_DeployMetadata.checkAsyncRequest';
+import {FlowAttributeChangeEvent, FlowNavigationNextEvent} from 'lightning/flowSupport';
+
+export default class TransferMetadata extends LightningElement {
+ 
+    contacts;
+    error;
+
+    @track jobId;
+
+    @track activity;
+    transferComplete = false;
+    @api zipFileString = '';
+    @api extractedMetadataString = '';
+    @api escapedMetadataString = '';
+    @api metadataName;
+    @api transferMode;
+    @api metadataString;
+    @api objectType;
+    modifiedName;
+    @api availableActions = [];
+
+
+    connectedCallback() {
+ 
+        this.activity = 'Beginning metadata transfer...';
+        if (this.transferMode.toLowerCase() == 'retrieve')
+            this.retrieve();
+        else if (this.transferMode.toLowerCase() == 'deploy')
+            this.deploy();
+            else {
+             console.log('transfer mode error!');
+            }
+    }
+
+    retrieve() {
+        console.log('beginning retrieval');
+        requestMetadata({ metadataName : this.metadataName })
+        .then(result => {
+            
+            console.log('successfully sent async retrieval request');
+            console.log('jobId is: ' + result);
+             
+            this.jobId = result;
+            
+            if (!this.transferComplete) {
+                console.log('initial retrieval not complete');
+                this.waitForRetrieval(this.jobId);
+            }
+        })
+        .catch(error => {
+            console.log('An error occurred on the initial retrieve call:');    
+            this.activity = 'Error: ' + JSON.stringify(error.body.message);
+            this.error = error;
+        });
+    }
+
+    deploy() {
+        console.log('beginning deployment');
+        console.log('this.metadataName is: ' + this.metadataName);
+        console.log('beginning deployment of this.metadataString is: ' + this.metadataString);
+        console.log('this.objectTpe is: ' + this.objectType);
+        if (this.objectType.toLowerCase() == this.objectType || this.objectType.toUpperCase() == this.objectType) {
+            console.log('ERROR: objectType parameter should be in CamelCase');
+        }
+        // this.modifiedName = this.metadataName + '_Converted';
+        this.modifiedName = this.metadataName;
+        console.log('this.metadataName is: ' + this.modifiedName);
+
+        // Special Processing to unescape characters that could be passed in by a flow
+        let documentContent = this.metadataString;
+        documentContent = documentContent.replaceAll('&lbrace;', '{');
+        documentContent = documentContent.replaceAll('&rbrace;', '}');
+
+        fbc_deployMetadata({ metadataText : documentContent, metadataName : this.modifiedName, testLevel: null, objectType : this.objectType,  })        
+        .then(result => {
+            console.log('result of deployment request is: ' + result);
+            console.log('successfully sent async deployment request');
+            console.log('jobId is: ' + result);
+             
+            this.jobId = result;
+            
+            if (!this.transferComplete) {
+                console.log('deployment not complete');
+                this.waitForDeployment(this.jobId);
+            } else {
+                this.activity = 'Deployment Complete!';
+                this.handleGoNext();
+            }
+        })
+        .catch(error => {
+            console.log('error calling fbc_deployMetadata from transfer');
+            this.activity = 'Error: ' + JSON.stringify(error.body.message);
+            this.error = error;
+        });
+    }
+    
+    handleGoNext() {
+        // check if NEXT is allowed on this screen
+        if (this.availableActions.find(action => action === 'NEXT')) {
+            // navigate to the next screen
+            const navigateNextEvent = new FlowNavigationNextEvent();
+            this.dispatchEvent(navigateNextEvent);
+        }
+    }
+
+    waitForRetrieval(jobId) {
+        setTimeout(function(){ 
+            console.log('checking status. jobId is: ' + this.jobId);
+            this.activity = 'Checking status...'
+            this.checkRetrievalStatus();
+        }.bind(this), 1000); 
+        this.activity = 'Waiting...'
+    }
+
+    waitForDeployment(jobId) {
+        setTimeout(function(){ 
+            console.log('checking status after wait. jobId is: ' + this.jobId);
+            this.activity = 'Checking status...'
+            self = this;
+            this.checkDeploymentStatus(self);
+        }.bind(this), 1000); 
+        this.activity = 'Waiting...'
+    }
+
+    checkDeploymentStatus(self) {
+        console.log('starting to check deploy status');
+        checkDeployStatus({ jobId : this.jobId })
+        .then(result => {
+            console.log('successfully checked deploy job status');
+            if (result == 'success'){
+                console.log('deployment successful');
+             
+                self.activity = 'Flow deployed successfully. '
+                console.log('this.activity is: ' + this.activity);
+                this.handleGoNext();
+
+            } else if (result == 'inprocess') {
+                console.log('deployment not complete');
+                this.waitForDeployment(this.jobId);
+                
+              } else  {
+                //console.log('not done yet. jobid is: ' + this.jobId);
+                console.log ('deployment failed');
+                console.log ( result);
+                self.activity = result;   
+            }
+        });
+ 
+    }
+
+    checkRetrievalStatus() {
+        console.log('starting to check retrieval status');
+        checkRetrieveStatus({ jobId : this.jobId })
+        .then(result => {
+            console.log('successfully checked job status');
+            if (result != 'inprocess'){
+                console.log('data returned');
+                console.log('data is: ' + result);
+                this.activity = 'metadata retrieved successfully. '
+                console.log('this.activity is: ' + this.activity);
+
+                let returnResults = JSON.parse(result);
+                this.zipFileString = returnResults.zipFile;
+                let attributeChangeEvent = new FlowAttributeChangeEvent('zipFileString', this.zipFileString);
+                this.dispatchEvent(attributeChangeEvent);                      
+
+                this.extractedMetadataString = returnResults.extractedFlowMetadata;
+                attributeChangeEvent = new FlowAttributeChangeEvent('extractedMetadataString', this.extractedMetadataString);
+                this.dispatchEvent(attributeChangeEvent);     
+
+                this.escapedMetadataString = returnResults.escapedFlowMetadata;
+                attributeChangeEvent = new FlowAttributeChangeEvent('escapedMetadataString', this.escapedMetadataString);
+                this.dispatchEvent(attributeChangeEvent);
+
+                const nextNavigationEvent = new FlowNavigationNextEvent();
+                this.dispatchEvent(nextNavigationEvent);
+            } else  {
+                console.log('retrieval not complete for job id: ' + this.jobId);
+                this.waitForRetrieval(this.jobId);
+            } 
+        })
+        .catch(error => {
+            console.log('error checking async request.');
+            this.error = error;
+            //retry endlessly
+            console.log('retrying');
+            this.waitForRetrieval(this.jobId);
+        });
+    }
+
+    handleChange(event) {
+        this.selectedFlowApiName = event.detail.value;
+        const attributeChangeEvent = new FlowAttributeChangeEvent('zipFileString', this.zipFileString);
+        this.dispatchEvent(attributeChangeEvent);
+    }
+
+}

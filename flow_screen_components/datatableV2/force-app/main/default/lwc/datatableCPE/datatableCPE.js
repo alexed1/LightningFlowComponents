cfaf54136644507f595e669b62835cdfc0742cb6
Alex Edelstein <aedelstein@aedelstein-ltm.internal.salesforce.com>
2020-10-17 19:15:33 -0700

    checkpoint datatable work

---
+++ flow_screen_components/datatableV2/force-app/main/default/lwc/datatableCPE/datatableCPE.js
@@ -0,0 +1,115 @@
+import {LightningElement, track, api} from 'lwc';
+
+
+const defaults = {
+    apexDefinedTypeInputs: false,
+    inputAttributePrefix: 'select_'
+
+};
+
+export default class DatatableCPE extends LightningElement {
+
+    _builderContext;
+    _values;
+
+    
+
+    @track inputValues = {
+        isUserDefinedObject: {value: null, valueDataType: null, isCollection: false, label: 'Input Records are Apex-Defined Objects'}
+
+
+    };
+
+    
+
+
+    @api get builderContext() {
+        return this._builderContext;
+    }
+
+    set builderContext(value) {
+        this._builderContext = value;
+    }
+
+    @api get inputVariables() {
+        return this._values;
+    }
+
+    set inputVariables(value) {
+
+        this._values = value;
+        this.initializeValues();
+    }
+
+    initializeValues(value) {
+        if (this._values && this._values.length) {
+            this._values.forEach(curInputParam => {
+                if (curInputParam.name && this.inputValues[curInputParam.name]) {
+                    this.inputValues[curInputParam.name].value = curInputParam.value;
+                    this.inputValues[curInputParam.name].valueDataType = curInputParam.valueDataType;
+                }
+            });
+        }
+        this.handleDefaultAttributes();
+    }
+
+    handleDefaultAttributes() {
+    
+    }
+
+    handleValueChange(event) {
+        if (event.target) {
+            let curAttributeName = event.target.name ? event.target.name.replace(defaults.inputAttributePrefix, '') : null;
+            let value = event.detail ? event.detail.value : event.target.value
+            let curAttributeValue = event.target.type === 'checkbox' ? event.target.checked : value;
+            let curAttributeType;
+            switch (event.target.type) {
+                case "checkbox":
+                    curAttributeType = 'Boolean';
+                    break;
+                case "number":
+                    curAttributeType = 'Number';
+                    break;
+                default:
+                    curAttributeType = 'String';
+            }
+            this.dispatchFlowValueChangeEvent(curAttributeName, curAttributeValue, curAttributeType);
+
+        }
+    }
+
+    handleFlowComboboxValueChange(event) {
+        if (event.target && event.detail) {
+            let changedAttribute = event.target.name.replace(defaults.inputAttributePrefix, '');
+            this.dispatchFlowValueChangeEvent(changedAttribute, event.detail.newValue, event.detail.newValueDataType);
+        }
+
+    }
+
+    dispatchFlowValueChangeEvent(id, newValue, newValueDataType) {
+        const valueChangedEvent = new CustomEvent('configuration_editor_input_value_changed', {
+            bubbles: true,
+            cancelable: false,
+            composed: true,
+            detail: {
+                name: id,
+                newValue: newValue ? newValue : null,
+                newValueDataType: newValueDataType
+            }
+        });
+        this.dispatchEvent(valueChangedEvent);
+    }
+
+
+
+    get isSObjectInput() {
+        if (this.inputValues.isUserDefinedObject) {
+            return this.inputValues.isUserDefinedObject.value === defaults.apexDefinedTypeInputs;
+        }
+
+    }
+
+    
+    
+
+}

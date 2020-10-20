bdb6cfa02072bf3e048b7a37d52d673d19043459
Eric Smith <ericrsmith35@gmail.com>
2020-10-20 15:55:51 -0400

    stll stuck on SObject collection variable

--- flow_screen_components/datatableV2/force-app/main/default/lwc/datatableCPE/datatableCPE.js
+++ flow_screen_components/datatableV2/force-app/main/default/lwc/datatableCPE/datatableCPE.js
@@ -7,10 +7,24 @@ const defaults = {
 
 export default class DatatableCPE extends LightningElement {
 
-    _builderContext;
-    _values;
+    _inputVariables = [];
+    _builderContext = [];
+    _elementInfo = {};
+    // _typeMappings = [];
 
     selectedSObject;
+
+    _flowVariables;
+    _elementType;
+    _elementName;
+
+    /* array of complex object containing name-value of a input parameter.
+     * eg: [{
+     *       name: 'prop1_name',
+     *       value: 'value',
+     *       valueDataType: 'string'
+     *     }]
+     */
 
     @track inputValues = {
         isUserDefinedObject: {value: false, valueDataType: null, isCollection: false, label: 'Input Records are Apex-Defined Objects'},
@@ -30,32 +44,58 @@ export default class DatatableCPE extends LightningElement {
         value: null
     }]
 
-    @api get builderContext() {
-        return this._builderContext;
+    @api 
+    get inputVariables() {
+        return this._inputVariables;
     }
 
-    set builderContext(value) {
-        this._builderContext = value;
-    }
-
-    @api get inputVariables() {
-        return this._values;
-    }
-
-    set inputVariables(value) {
-        this._values = value;
+    set inputVariables(variables) {
+        this._inputVariables = variables || [];
         this.initializeValues();
     }
 
-    initializeValues(value) {
-        if (this._values && this._values.length) {
-            this._values.forEach(curInputParam => {
+    @api 
+    get builderContext() {
+        return this._builderContext;
+    }
+
+    set builderContext(context) {
+        this._builderContext = context || {};
+        if (this._builderContext) {
+            const { variables } = this._builderContext;
+            this._flowVariables = [...variables];
+        }
+    }
+
+    /* contains the information about the LWC or Action in which
+   * the configurationEditor is defined.
+   * eg: {
+   *       apiName: 'CreateCase', // dev name of the action or screen
+   *       type: 'Action' // or 'Screen'
+   *     }
+   */
+    @api
+    get elementInfo() {
+        return this._elementInfo;
+    }
+
+    set elementInfo(info) {
+        this._elementInfo = info || {};
+        if (this._elementInfo) {
+            this._elementName = this._elementInfo.apiName;
+            this._elementType = this._elementInfo.type;
+        }
+    }
+
+    initializeValues() {
+        this._inputVariables.forEach(curInputParam => {
+            if (curInputParam.name && curInputParam.value) {
                 if (curInputParam.name && this.inputValues[curInputParam.name]) {
-                    this.inputValues[curInputParam.name].value = curInputParam.value;
+                    this.inputValues[curInputParam.name].value = (curInputParam.valueDataType === 'reference') ? '{!' + curInputParam.value + '}' : curInputParam.value;
                     this.inputValues[curInputParam.name].valueDataType = curInputParam.valueDataType;
                 }
-            });
-        }
+            }
+        });
         this.handleDefaultAttributes();
     }
 
@@ -83,11 +123,43 @@ export default class DatatableCPE extends LightningElement {
         }
     }
 
+    /* array of complex object containing type name-value of the dynamic data types
+   * in Action or Screen
+   * eg: [{
+   *       typeName: 'T', // the type name
+   *       typeValue: 'Account' // or any other sObject
+   *     }]
+   */
+    
+    /*     
+    @api
+    get typeMappings() {
+        return this._typeMappings;
+    }
+
+    set typeMappings(mappings) {
+        this._typeMappings = mappings || {};
+        this.initializeTypeMappings();
+    } */
+    
+    /* Return a promise that resolve and return errors if any
+   *    [{
+   *      key: 'key1',
+   *      errorString: 'Error message'
+   *    }]
+   */
+    @api
+    validate() {
+        const validity = [];
+        return validity;
+    }
+
     handleDynamicTypeMapping(event) { 
         console.log('handling a dynamic type mapping');
         console.log('event is ' + JSON.stringify(event));
         let typeValue = event.detail.objectType;
-        let typeName = 'T'; //this is hardcoded, which is bad, and should be a lookup to a setting. 
+        // let typeName = 'T'; //this is hardcoded, which is bad, and should be a lookup to a setting.
+        const typeName = this._elementType === "Screen" ? 'T' : 'T__record'; 
         console.log('typeValue is: ' + typeValue);
         const dynamicTypeMapping = new CustomEvent('configuration_editor_generic_type_mapping_changed', {
             composed: true,
@@ -99,8 +171,23 @@ export default class DatatableCPE extends LightningElement {
             }
         });
         this.dispatchEvent(dynamicTypeMapping);
+        this.updateRecordVariablesComboboxOptions(typeValue);
         this.selectedSObject = typeValue;
         this.updateFlowParam('vSObject', typeValue);
+    }
+
+    updateRecordVariablesComboboxOptions(objectType) {
+        const variables = this._flowVariables.filter(
+            (variable) => variable.objectType === objectType
+        );
+        let comboboxOptions = [];
+        variables.forEach((variable) => {
+            comboboxOptions.push({
+                label: variable.name,
+                value: "{!" + variable.name + "}"
+            });
+        });
+        return comboboxOptions;
     }
 
     handleFlowComboboxValueChange(event) {
@@ -153,7 +240,7 @@ export default class DatatableCPE extends LightningElement {
     }
 
     handleFlowStatusChange(event) {
-        console.log('STATUS CHANGE', event.detail.flowParams);
+        console.log('STATUS CHANGE', event.detail.flowParams);  // These are values coming back from the Wizard Flow
     }
 
     //don't forget to credit https://www.salesforcepoint.com/2020/07/LWC-modal-popup-example-code.html

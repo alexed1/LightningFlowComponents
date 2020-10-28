a3ce81213a8be97e50f8b59b32e4a4bbe8084d6c
ehretg <ehretg@gmail.com>
2020-10-28 10:08:39 +0100

    Create ChatSessionManagerLAController.js

---
+++ flow_action_components/ChatSessionManagerLA/ChatSessionManagerLAController.js
@@ -0,0 +1,14 @@
+({
+    invoke : function(component, event, helper) {
+        var actionType = component.get("v.actionType");
+        
+        if (actionType == 'sendMessage')
+            helper.sendMessage(component, event);
+        if (actionType == 'setAgentInput')
+            helper.setAgentInput(component, event);
+        if (actionType == 'endChat')
+            helper.endChat(component, event);
+        if (actionType == 'initFileTransfer')
+            helper.initFileTransfer(component, event);
+    }
+})

883aa9e135d2d44a25b0810f64a52e92a543b557
ehretg <ehretg@gmail.com>
2020-11-02 10:12:06 +0100

    Update ChatSessionManagerSCcontroller.js

--- flow_screen_components/ChatSessionManagerSC/ChatSessionManagerSCcontroller.js
+++ flow_screen_components/ChatSessionManagerSC/ChatSessionManagerSCcontroller.js
@@ -4,11 +4,11 @@
 
         if (actionType == 'sendMessage')
             helper.sendMessage(component, event);
-        if (actionType == 'setAgentInput')
-            helper.setAgentInput(component, event);
+        if (actionType == 'loadMessage')
+            helper.loadMessage(component, event);
         if (actionType == 'endChat')
             helper.endChat(component, event);
-        if (actionType == 'initFileTransfer')
-            helper.initFileTransfer(component, event);
-    }
+        if (actionType == 'requestFile')
+            helper.requestFile(component, event);
+    },
 })

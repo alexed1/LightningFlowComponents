9db4cce82730c649070c04512b15aff5e1d01236
ehretg <ehretg@gmail.com>
2020-11-02 10:21:18 +0100

    Update ChatSessionManagerSCcontroller.js

--- flow_screen_components/ChatSessionManagerSC/ChatSessionManagerSCcontroller.js
+++ flow_screen_components/ChatSessionManagerSC/ChatSessionManagerSCcontroller.js
@@ -1,14 +1,20 @@
 ({
     doInit : function(component, event, helper) {
         var actionType = component.get("v.actionType");
+        var sessionId = component.get("v.sessionId");
+        var sessionIdShort = (sessionId.length > 15 ? sessionId.substring(0, 15) : sessionId);
 
         if (actionType == 'sendMessage')
-            helper.sendMessage(component, event);
+            helper.sendMessage(component, event, sessionIdShort);
         if (actionType == 'loadMessage')
-            helper.loadMessage(component, event);
+            helper.loadMessage(component, event, sessionIdShort);
         if (actionType == 'endChat')
-            helper.endChat(component, event);
-        if (actionType == 'requestFile')
-            helper.requestFile(component, event);
+            helper.endChat(component, event, sessionIdShort);
+        if (actionType == 'requestFile' && recordId != null && recordId != '') {
+            var recordId = component.get("v.recordId");
+            var recordIdShort = (recordId.length > 15 ? recordId.substring(0, 15) : recordId);
+            
+            helper.requestFile(component, event, sessionIdShort, recordIdShort);
+        }
     },
 })

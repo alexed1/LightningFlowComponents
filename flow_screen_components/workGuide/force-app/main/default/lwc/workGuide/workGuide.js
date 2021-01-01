c3a3d9151bfb9c826f8a0f90da163b01a1a722bf
Alex Edelstein <aedelstein@salesforce.com>
2021-01-01 11:13:28 -0800

    misc

--- flow_screen_components/workGuide/force-app/main/default/lwc/workGuide/workGuide.js
+++ flow_screen_components/workGuide/force-app/main/default/lwc/workGuide/workGuide.js
@@ -57,8 +57,11 @@ export default class WorkGuide extends LightningElement {
             this.isLoadCompleted = true;
         } else if (typeof response.data !== undefined) {
             if (response.data) {
-                console.log('response from getActiveWorkItemsByRecordId is: ' + JSON.stringify(response.data));
-
+                //console.log('response from getActiveWorkItemsByRecordId is: ' + JSON.stringify(response.data));
+                //assemble combination strings from the labels and record ids
+                //set those as the value used by the picklist
+                //exit
+                //in the html if the attribute has a value, show it
                 this.curWorkItems = response.data.workItemRecordIds;
 
                 console.log('now calling getWorkItemDetail');
@@ -76,7 +79,7 @@ export default class WorkGuide extends LightningElement {
                                         label: 'orchdef',
                                         link: this.url
                                     };
-                                    console.log('values: appProcess ' + this.appProcess);
+                                    //console.log('values: appProcess ' + JSON.stringify(this.appProcess));
                                     this._currentStepName = result.currentStep__c;
                                     this._currentStage = result.currentStage__c;
                                     this._currentDefinitionId = 'foo';
@@ -85,10 +88,10 @@ export default class WorkGuide extends LightningElement {
                                     this._currentWorkItemId = this.curWorkItems[0];
                                     this.stageToStepsNameMap = JSON.parse(result.StageStepMapping__c);
                                     this.stepToFlowMap = JSON.parse(result.StepFlowMapping__c);
-                                    console.log('values: currentStep ' + this._currentStepName);
-                                    console.log('values: stepToFlowMap ' + this.stepToFlowMap);
-                                    console.log('values: _currentOrchInstanceId ' + this._currentOrchInstanceId);
-                                    console.log('values: _currentOrchStepInstanceId ' + this._currentOrchStepInstanceId);
+                                    //console.log('values: _currentStepName ' + this._currentStepName);
+                                    //console.log('values: _currentStage ' + this._currentStage);
+                                    //console.log('values: _currentOrchInstanceId ' + this._currentOrchInstanceId);
+                                    //console.log('values: _currentOrchStepInstanceId ' + this._currentOrchStepInstanceId);
                                 } else {
                                     console.log('typeof check failed');
                                 }
@@ -110,29 +113,7 @@ export default class WorkGuide extends LightningElement {
 
     }
 
-    /* @wire(getWorkItemDetail, {userId: '$curUserId', contextRecordId: '$contextRecordId', workItemRecordId: '$curWorkItems'})
-    _getWorkItemDetail(response) {
-        console.log('getWorkItemDetail returning....');
-        if (response.error) {
-            console.log(JSON.stringify(response.error));
-            this.isLoadCompleted = true;
-        } else if (typeof response.data !== undefined) {
-            if (response.data) {
-                this.appProcess = {
-                    label: response.data.App_Process_Definition__r.Name,
-                    link: this.url + '/' + response.data.App_Process_Definition__c
-                };
-                this._currentStepName = response.data.currentStep__c;
-                this._currentStage = response.data.currentStage__c;
-                this._currentDefinitionId = response.data.App_Process_Definition__c;
-                this._currentOrchInstanceId = response.data.Id;
-                this.stageToStepsNameMap = JSON.parse(response.data.App_Process_Definition__r.StageStepMappings__c);
-                this.stepToFlowMap = JSON.parse(response.data.App_Process_Definition__r.StepFlowMappings__c);
-            }
-            this.isLoadCompleted = true;
-        }
-
-    } */
+  
 
     get curFlowName() {
         if (this.stepToFlowMap && this.stepToFlowMap[this._currentStepName]) {
@@ -141,6 +122,9 @@ export default class WorkGuide extends LightningElement {
     }
 
     get isNoWorkAvailable() {
+        //console.log('this.isLoadCompleted is: ' + this.isLoadCompleted);
+        //console.log('this.stepToFlowMap is: ' + JSON.stringify(this.stepToFlowMap));
+        //console.log('this._currentStepName is ' + this._currentStepName);
         return (this.isLoadCompleted && (!this.stepToFlowMap || !this.stepToFlowMap[this._currentStepName]));
     }
 
@@ -148,13 +132,17 @@ export default class WorkGuide extends LightningElement {
         let passedCurrent = false;
         if (this.stageToStepsNameMap) {
             let pathStructure = [];
+            //console.log('stagetostepsnamemap is: ' + JSON.stringify(this.stageToStepsNameMap));
             Object.keys(this.stageToStepsNameMap).forEach(curStage => {
                 let stageClass = 'slds-is-incomplete';
+                //console.log('this._currentstage is: ' + this._currentStage + 'and curStage is: ' + curStage);
                 if (this._currentStage) {
                     if (!passedCurrent && curStage === this._currentStage) {
+                        console.log('setting passedCurrent to true for curStage:' + curStage);
                         passedCurrent = true;
                     }
                     let isCurrent = curStage === this._currentStage;
+                    
                     stageClass = isCurrent ? 'slds-is-current slds-is-active' : (passedCurrent ? 'slds-is-incomplete' : 'slds-is-complete');
                 }
                 pathStructure.push({
@@ -162,6 +150,7 @@ export default class WorkGuide extends LightningElement {
                     stageClass: 'slds-path__item ' + stageClass
                 });
             });
+            //console.log('pathStructure is: ' + JSON.stringify(pathStructure));
             return pathStructure;
         }
     }

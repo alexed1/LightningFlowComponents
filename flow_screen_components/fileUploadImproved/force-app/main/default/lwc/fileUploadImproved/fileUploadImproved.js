f031a797877f2575c879e1a13b15101c5c7913aa
jdayment <52637666+jdayment@users.noreply.github.com>
2021-05-31 08:06:03 -0600

    Update fileUploadImproved.js

--- flow_screen_components/fileUploadImproved/force-app/main/default/lwc/fileUploadImproved/fileUploadImproved.js
+++ flow_screen_components/fileUploadImproved/force-app/main/default/lwc/fileUploadImproved/fileUploadImproved.js
@@ -9,21 +9,14 @@ export default class FileUpload extends LightningElement {
     @api acceptedFormats;
     @track lstAllFiles = [];
     @track docIds =[];
-   
- 
-    get acceptedFormats() {
-        return ['.pdf','.png','.jpg', '.docx','.doc', '.csv','.xlsx'];
-    }
- 
+
+    
     handleUploadFinished(event) {
         // Get the list of uploaded files
         const lstUploadedFiles = event.detail.files;
         lstUploadedFiles.forEach(fileIterator => this.lstAllFiles.push(fileIterator.name));
         lstUploadedFiles.forEach(fileIterator => this.docIds.push(fileIterator.documentId));
-        console.log(this.contentDocumentIds=this.docIds);
-
-
-        
+        console.log(this.contentDocumentIds=this.docIds);     
         
         
 

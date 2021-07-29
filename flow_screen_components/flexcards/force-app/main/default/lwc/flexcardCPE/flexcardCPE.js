12dced92a872b96fa5b5b7c763fa110d9bd96168
jdayment <52637666+jdayment@users.noreply.github.com>
2021-07-29 15:52:58 -0700

    Update flexcardCPE.js

--- flow_screen_components/flexcards/force-app/main/default/lwc/flexcardCPE/flexcardCPE.js
+++ flow_screen_components/flexcards/force-app/main/default/lwc/flexcardCPE/flexcardCPE.js
@@ -15,7 +15,7 @@ export default class FlexcardCPE extends LightningElement {
         isClickable: {value: null, valueDataType: null, isCollection: false, label: 'Set as true if you wish to select individual cards for action further downstream in flow default is false'},
         headerStyle: {value: null, valueDataType: null, isCollection: false, label: 'Add your own style attribute to the card headers ie. background-color:red;'},
         allowMultiSelect: {value: null, valueDataType: null, isCollection: false, label: 'Allow for multiselect of cards when enabled checkboxes appear on cards and adds selected cards to collection'},
-        objectAPIName: {value: null, valueDataType: null, isCollection: false, label: 'The SObject API Name used to query fields and values must be the same object selected in Flexcard Object API Name'},
+        objectAPIName: {value: null, valueDataType: null, isCollection: false, label: 'Object Name'},
         label: {value: null, valueDataType: null, isCollection: false, label: 'Enter a label for you component'},
         transitionOnClick: {value: null, valueDataType: null, isCollection: false, label: 'If marked as true will transition flow to next screen on card click'},
         

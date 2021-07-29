de812d1c19c09e30b1ea54a6af9dda35046b2361
jdayment <52637666+jdayment@users.noreply.github.com>
2021-07-29 15:35:30 -0700

    Update flexcardCPE.js

--- flow_screen_components/flexcards/force-app/main/default/lwc/flexcardCPE/flexcardCPE.js
+++ flow_screen_components/flexcards/force-app/main/default/lwc/flexcardCPE/flexcardCPE.js
@@ -4,8 +4,9 @@ export default class FlexcardCPE extends LightningElement {
     _builderContext;
     _values;
 
+
     @track inputValues = {
-        value: {value: null, valueDataType: null, isCollection: false, label: 'The selected cards recordId if Allow Multi-Select is set to false'},
+        value: {value: null, valueDataType: null, isCollection: false, label: 'The selected cards recordId'},
         icon: {value: null, valueDataType: null, isCollection: false, label: 'Icon name for example standard:account'},
         records: {value: null, valueDataType: null, isCollection: true, label: 'Record Collection variable containing the records to display in the flexcard.'},
         visibleFieldNames: {value: null, valueDataType: null, isCollection: false, label: 'Show which fields?'},
@@ -19,5 +20,75 @@ export default class FlexcardCPE extends LightningElement {
         transitionOnClick: {value: null, valueDataType: null, isCollection: false, label: 'If marked as true will transition flow to next screen on card click'},
         
         };
+
+        @api get builderContext() {
+            return this._builderContext;
+        }
+    
+        set builderContext(value) {
+            this._builderContext = value;
+        }
+    
+        @api get inputVariables() {
+            return this._values;
+        }
+    
+        set inputVariables(value) {
+    
+            this._values = value;
+            this.initializeValues();
+        }
+    
+        initializeValues(value) {
+            if (this._values && this._values.length) {
+                this._values.forEach(curInputParam => {
+                    if (curInputParam.name && this.inputValues[curInputParam.name]) {
+                        this.inputValues[curInputParam.name].value = curInputParam.value;
+                        this.inputValues[curInputParam.name].valueDataType = curInputParam.valueDataType;
+                    }
+                });
+            }
+
+        }
+
+        handleFlowComboboxValueChange(event) {
+            if (event.target && event.detail) {
+                this.dispatchFlowValueChangeEvent(event.target.name.replace(this.settings.inputAttributePrefix, ''), event.detail.newValue, event.detail.newValueDataType);
+            }
+        }
+
+        handleValueChange(event) {
+            if (event.target) {
+                let curAttributeName = event.target.name ? event.target.name.replace(this.settings.inputAttributePrefix, '') : null;
+                let curAttributeValue = event.target.type === 'checkbox' ? event.target.checked : event.detail.value;
+                let curAttributeType;
+                switch (event.target.type) {
+                    case "checkbox":
+                        curAttributeType = 'Boolean';
+                        break;
+                    case "number":
+                        curAttributeType = 'Number';
+                        break;
+                    default:
+                        curAttributeType = 'String';
+                }
+                this.dispatchFlowValueChangeEvent(curAttributeName, curAttributeValue, curAttributeType);
+            }
+        }
+
+        dispatchFlowValueChangeEvent(id, newValue, dataType) {
+            const valueChangedEvent = new CustomEvent('configuration_editor_input_value_changed', {
+                bubbles: true,
+                cancelable: false,
+                composed: true,
+                detail: {
+                    name: id,
+                    newValue: newValue ? newValue : null,
+                    newValueDataType: dataType
+                }
+            });
+            this.dispatchEvent(valueChangedEvent);
+        }
+    
 
 }

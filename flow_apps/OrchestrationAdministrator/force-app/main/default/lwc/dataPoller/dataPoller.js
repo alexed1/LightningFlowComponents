0e021eaea0e6be7e86ae8fc9be5aebe47f6afbfd
Sahib Gadzhiev <sahib.gadzhiev@jetbi.com>
2021-08-18 14:04:36 +0300

    Updated dataPoller lwc

--- flow_apps/OrchestrationAdministrator/force-app/main/default/lwc/dataPoller/dataPoller.js
+++ flow_apps/OrchestrationAdministrator/force-app/main/default/lwc/dataPoller/dataPoller.js
@@ -1,5 +1,6 @@
 import { api, LightningElement } from 'lwc';
 import getSobjects from '@salesforce/apex/DataPollerController.getSobjects';
+import { FlowAttributeChangeEvent } from 'lightning/flowSupport';
 export default class DataPoller extends LightningElement {
     @api pollingFrequency = 3;
     @api queryString;
@@ -7,13 +8,18 @@ export default class DataPoller extends LightningElement {
     @api error;
 
     connectedCallback() {
+        console.log('queryString', this.queryString);
+        console.log('pollingFrequency', this.pollingFrequency);
         setInterval(function() {
             console.log('queryString', this.queryString);
-            console.log('pollingFrequency', this.pollingFrequency);
+            console.log('pollingFrequency', this.pollingFrequency,JSON.parse(JSON.stringify(this.retrievedRecords)));
             getSobjects({queryString : this.queryString}).then(
                 result => {
                     console.log('result ', result);
+                    
                     this.retrievedRecords = result;
+                    this.dispatchEvent(new FlowAttributeChangeEvent('retrievedRecords', this.retrievedRecords));    //JSON Version
+                    console.log('result2 ', JSON.parse(JSON.stringify(this.retrievedRecords)));
                 }
             ).catch(
                 error => {
@@ -21,6 +27,16 @@ export default class DataPoller extends LightningElement {
                     console.error('error', error);
                 }
             );
-        }, this.pollingFrequency * 1000)
+        }.bind(this), this.pollingFrequency * 1000
+        )
+        
+    }
+
+    refresh(){
+        console.log('queryString', this.queryString);
+        console.log('pollingFrequency', this.pollingFrequency);
+        console.log('retrievedRecords', JSON.parse(JSON.stringify(this.retrievedRecords)));
+        
+        
     }
 }

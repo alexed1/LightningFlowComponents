beb4bd29182aaa66017eef6ef0f78bf8e6467e66
jdayment <52637666+jdayment@users.noreply.github.com>
2021-09-21 09:27:14 -0700

    Update flexcardFlow.js

--- flow_screen_components/flexcards/force-app/main/default/lwc/flexcardFlow/flexcardFlow.js
+++ flow_screen_components/flexcards/force-app/main/default/lwc/flexcardFlow/flexcardFlow.js
@@ -23,7 +23,7 @@ export default class FlexcardFlow extends LightningElement {
     @api label;
     @api transitionOnClick;
     @api availableActions = [];
-@api Cardcss;
+    @api Cardcss;
     @track Cardcss;
     @track fieldHTML='';
     @track recordLayoutData={};

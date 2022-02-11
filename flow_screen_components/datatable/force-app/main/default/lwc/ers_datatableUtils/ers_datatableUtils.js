91fec929bf04c94addd380ca402e36d26efab606
Eric Smith <ericrsmith35@gmail.com>
2022-02-11 17:30:27 -0500

    Experience url fix

--- flow_screen_components/datatable/force-app/main/default/lwc/ers_datatableUtils/ers_datatableUtils.js
+++ flow_screen_components/datatable/force-app/main/default/lwc/ers_datatableUtils/ers_datatableUtils.js
@@ -15,12 +15,13 @@ if (baseURL.includes('--c.visualforce.')) {     // Running in Flow Builder
     myDomain = 'https://' + window.location.hostname.split('.')[0];
 } else {                                        // Running in a Community
     myDomain = window.location.href;            // https://<domain>.<instance>.force.com/<site title>/s/
+    myDomain = myDomain.split('/s/')[0] + '/s/';    // v3.4.5 Remove everything after the /s/ (non-home pages)
     isCommunity = true;
 }
 
 const getConstants = () => {
     return {
-        VERSION_NUMBER : '3.3.2',   // Current Source Code Version #
+        VERSION_NUMBER : '3.4.5',   // Current Source Code Version #
         MAXROWCOUNT : 1000,         // Limit the total number of records to be handled by this component
         ROUNDWIDTH : 5,             // Used to round off the column widths during Config Mode to nearest value
         WIZROWCOUNT : 6,            // Number of records to display in the Column Wizard datatable

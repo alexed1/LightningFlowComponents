697d1947c56f148718d79b63f479c49e6c074ebf
Sahib Gadzhiev <sahib.gadzhiev@jetbi.com>
2022-09-13 11:59:32 +0200

    Adjusted lwc

--- flow_apps/WorkGuidePlus/force-app/main/default/lwc/workGuidePlus/workGuidePlus.js
+++ flow_apps/WorkGuidePlus/force-app/main/default/lwc/workGuidePlus/workGuidePlus.js
@@ -1,16 +1,21 @@
 import { LightningElement } from 'lwc';
 import publishFlowOrchestrationEvent from '@salesforce/apex/WorkGuidePlusController.publishFlowOrchestrationEvent'
-const FINISH_STATUS = 'FINISHED';
+const FINISHED_STATUS = 'FINISHED';
+const FINISHED_SCREEN_STATUS = 'FINISHED_SCREEN';
 const SHOW_WORK_ITEM_NAME = 'ShowWorkItems';
+const WORK_GUIDE_LABEL = 'Work Guide Plus';
+const COMPLETED_STATUS = 'Completed';
 
 export default class WorkGuidePlus extends LightningElement {
     orchestrationStepId;
     workItemId;
     screenFlowAPIName;
     showWorkItemsFlowName = SHOW_WORK_ITEM_NAME;
+    workGuideTitle = WORK_GUIDE_LABEL
     screenFlowInputs = '';
     orchestrationStepRun;
     workItemIsSelected = false;
+    showSpinner = false;
     get flowInputs() {
         if(this.screenFlowInputs) {
             return JSON.parse(this.screenFlowInputs);
@@ -19,41 +24,44 @@ export default class WorkGuidePlus extends LightningElement {
         }
     }
     handleShowWorkItemsStatusChange(event) {
-        if(event.detail.status === FINISH_STATUS) {
+        if(event.detail.status === FINISHED_STATUS || event.detail.status === FINISHED_SCREEN_STATUS) {
             event.detail.outputVariables.forEach(
                 item => {
                     this[item.name] = item.value;
                 }
             );
-            //this.showWorkItemsFlowName = '';
             this.workItemIsSelected = true;
         }
         console.log(event);
-        // console.log(event.detail.currentStage);
-        // console.log(event.detail.flowTitle);
-        // console.log(event.detail.outputVariables);
-        // console.log(event.detail.status);
 
     }
 
     handleScreenFlowStatusChange(event) {
-        if(event.detail.status === FINISH_STATUS) {
+        if(event.detail.status === FINISHED_STATUS || event.detail.status === FINISHED_SCREEN_STATUS) {
+            this.showSpinner =  true;
             publishFlowOrchestrationEvent(
                 {
                     stepInstanceId : this.orchestrationStepId,
                     orchestrationInstanceId : this.orchestrationStepRun,
-                    eventPayload : JSON.stringify(event.detail.outputVariables),
-                    stepStatus : 'Completed'
+                    eventPayload : event.detail.outputVariables ? JSON.stringify(event.detail.outputVariables) : '',
+                    stepStatus : COMPLETED_STATUS
                 }
             ).then(
                 () => {
-                    console.log('success');
-                    this.workItemIsSelected = false;
-                    //this.showWorkItemsFlowName = SHOW_WORK_ITEM_NAME;
+                    setTimeout(
+                        () => {
+                            this.showSpinner = false;
+                            this.workItemIsSelected = false;
+                        }, 3000
+                    );
                 }
             ).catch(
                 () => {console.log('error')}
             );
         }
+    }
+
+    showWorkItem(event) {
+        this.workItemIsSelected = false;
     }
 }

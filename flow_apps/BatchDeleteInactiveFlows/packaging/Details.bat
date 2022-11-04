a6081dcf64f444ab8cab5800ee6d9d0b793dada5
Eric Smith <ericrsmith35@gmail.com>
2022-11-04 17:36:37 -0400

    packaging updates

--- flow_apps/BatchDeleteInactiveFlows/packaging/Details.bat
+++ flow_apps/BatchDeleteInactiveFlows/packaging/Details.bat
@@ -2,4 +2,4 @@
 if "%1" neq "" set packageName=%1
 if "%2" neq "" set version=%2
 @echo on
-sfdx force:package:version:report -v lexhost --package "%packageName%@%version%-0"
+sfdx force:package:beta:version:report -v lexhost --package "%packageName%@%version%-0"

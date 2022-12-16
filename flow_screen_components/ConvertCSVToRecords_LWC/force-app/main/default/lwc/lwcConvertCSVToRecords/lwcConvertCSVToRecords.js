e0ab0fcf739bdb92b0b38c33e5ec723af027ab0f
Andy Haas <andyhaas@MacBook-Pro-2.local>
2022-12-16 09:10:01 -0600

    added cpe, added field name cleanup, fixed bugs

---
+++ flow_screen_components/ConvertCSVToRecords_LWC/force-app/main/default/lwc/lwcConvertCSVToRecords/lwcConvertCSVToRecords.js
@@ -0,0 +1,434 @@
+import { LightningElement, track, api } from 'lwc';
+import {
+	FlowNavigationFinishEvent,
+	FlowNavigationNextEvent,
+	FlowAttributeChangeEvent
+} from "lightning/flowSupport";
+import getObjectFields from '@salesforce/apex/LwcConvertCSVToRecordsHelper.getObjectFields';
+import { loadScript } from 'lightning/platformResourceLoader';
+import PARSER from '@salesforce/resourceUrl/PapaParse';
+
+export default class lwcConvertCSVToRecords extends LightningElement {
+	// Initialize the parser
+	parserInitialized = false;
+
+	// PapaParser Inputs
+	@api get delimiter() {
+		return this._delimiter;
+	}
+	set delimiter(value) {
+		this._delimiter = value;
+	}
+	_delimiter = ',';
+
+	@api get newline() {
+		return this._newline;
+	}
+	set newline(value) {
+		this._newline = value;
+	}
+	_newline = '';
+	
+	@api get quoteChar() {
+		return this._quoteChar;
+	}
+	set quoteChar(value) {
+		this._quoteChar = value;
+	}
+	_quoteChar = '"';
+	
+	@api get escapeChar() {
+		return this._escapeChar;
+	}
+	set escapeChar(value) {
+		this._escapeChar = value;
+	}
+	_escapeChar = '"';
+
+	@api get transformHeader() {
+		return this._transformHeader;
+	}
+	set transformHeader(value) {
+		this._transformHeader = value;
+	}
+	_transformHeader = undefined;
+	
+	@api get header() {
+		return this._header;
+	}
+	set header(value) {
+		this._header = value;
+	}
+	_header = false;
+	
+	@api get dynamicTyping() {
+		return this._dynamicTyping;
+	}
+	set dynamicTyping(value) {
+		this._dynamicTyping = value;
+	}
+	_dynamicTyping = false;
+	
+	@api get encoding() {
+		return this._encoding;
+	}
+	set encoding(value) {
+		this._encoding = value;
+	}
+	_encoding = '';
+	
+	@api get comments() {
+		return this._comments;
+	}
+	set comments(value) {
+		this._comments = value;
+	}
+	_comments = false;
+	
+	@api get fastMode() {
+		return this._fastMode;
+	}
+	set fastMode(value) {
+		this._fastMode = value;
+	}
+	_fastMode = false;
+	
+	@api get skipEmptyLines() {
+		return this._skipEmptyLines;
+	}
+	set skipEmptyLines(value) {
+		this._skipEmptyLines = value;
+	}
+	_skipEmptyLines = false;
+	
+	@api get transform() {
+		return this._transform;
+	}
+	set transform(value) {
+		this._transform = value;
+	}
+	_transform = undefined;
+	
+	@api get delimitersToGuess() {
+		return this._delimitersToGuess;
+	}
+	set delimiterToGuess(value) {
+		this._delimitersToGuess = value;
+	}
+	_delimitersToGuess = [];
+
+	// Get Label for Input file
+	@api get inputLabel() {
+		return this._inputLabel;
+	}
+	set inputLabel(value) {
+		this._inputLabel = value;
+	}
+	_inputLabel = 'Upload CSV';
+
+	// Get flow attributes
+	@api get availableActions() {
+		return this._availableActions;
+	}
+	set availableActions(value) {
+		this._availableActions = value;
+	}
+	_availableActions = [];
+
+	@api get autoNavigateNext() {
+		return this._autoNavigateNext;
+	}
+	set autoNavigateNext(value) {
+		this._autoNavigateNext = value;
+	}
+	_autoNavigateNext = false;
+	
+	// Set variables for the screen
+	@track uploadFileName = '';
+	@track uploadedFile = [];
+	
+	// Set objectName variable to be used in the getObjectFields Apex method
+	@api objectName = 'Account';
+
+	// Set the isError variable to false
+	@api get isError() {
+		return this._isError;
+	}
+	set isError(value) {
+		this._isError = value;
+	}
+	_isError = false;
+
+	// Set the errorMessage variable to an null 
+	@api get errorMessage() {
+		return this._errorMessage;
+	}
+	set errorMessage(value) {
+		this._errorMessage = value;
+	}
+	_errorMessage = null;
+	
+	// Store the fileName of the uploaded CSV file
+	@track fileName = '';
+
+	// Store the fields for the selected object
+	@track objectInfo = [];
+
+	// Store Column Headers
+	@track columnHeaders = [];
+
+	// Store Rows of Data
+	@track rows = [];
+
+	// Store the output SObject records from the CSV file
+	@api get outputValue() {
+		return this._outputValue;
+	}
+	set outputValue(value) {
+		this._outputValue = value;
+	}
+	_outputValue = [];
+
+	// Store a Status field to show the user the status of the CSV file
+	@track uploadFileStatus = '';
+
+	// Set isLoading to false
+	@api get isLoading() {
+		return this._isLoading;
+	}
+	set isLoading(value) {
+		this._isLoading = value;
+	}
+	_isLoading = false;
+
+
+	// Initialize the parser
+	renderedCallback() {
+		if(!this.parserInitialized){
+				loadScript(this, PARSER)
+						.then(() => {
+								this.parserInitialized = true;
+						})
+						.catch(error => console.error(error));
+		}
+	}
+
+	handleInputChange(event){
+		// Set Defualt Values
+		this.header = true;
+		this.skipEmptyLines = true;
+		
+		if(event.detail.files.length > 0){
+				this._isLoading = true;
+				const file = event.detail.files[0];
+				this.loading = true;
+				Papa.parse(file, {
+						delimiter: this._delimiter,
+						newline: this._newline,
+						quoteChar: this._quoteChar,
+						escapeChar: this._escapeChar,
+						transformHeader: this._transformHeader,
+						header: this._header,
+						dynamicTyping: this._dynamicTyping,
+						encoding: this._encoding,
+						comments: this._comments,
+						fastMode: this._fastMode,
+						skipEmptyLines: this._skipEmptyLines,
+						transform: this._transform,
+						delimitersToGuess: this._delimitersToGuess,
+						complete: (parsedResults) => {
+								console.log('results: ' + JSON.stringify(parsedResults));
+
+								// get the medta columns
+								this._columnHeaders = parsedResults.meta.fields;
+								console.log('columnHeaders: ' + JSON.stringify(this._columnHeaders));
+
+								console.log('objectName: ' + this.objectName);
+								
+								getObjectFields({objectName: this.objectName})
+								.then(fieldList => {
+										console.log('fieldList: ' + typeof fieldList);
+
+										// Set new columnHeader array
+										let newColumnHeaders = [];
+
+										// Set array of fields to remove
+										let fieldsToRemove = [];
+
+										// Compare the column headers to the fields for the selected object
+										// If the column header is not a match add __c to the end and recheck the fields
+										// If the column header is still not a match, remove the column header from the list
+										for (let i = 0; i < this._columnHeaders.length; i++) {
+											let columnHeader = this._columnHeaders[i];
+
+											// Trim the column header
+											columnHeader = columnHeader.trim();
+
+											console.log('columnHeader: ' + columnHeader);
+
+											// For standard fields we need to remove the space inbetween the words
+											// For example: Account Name becomes AccountName
+											// Create standardField variable to store the new value
+											let standardField;
+											if (columnHeader.includes(' ')) {
+												standardField = columnHeader.replaceAll(' ', '');
+											} else {
+												standardField = columnHeader;
+											}
+											if (fieldList.includes(standardField)) {
+												console.log('standard field: ' + columnHeader);
+												newColumnHeaders.push({"newField":columnHeader, "oldField":columnHeader});
+											} else {
+											
+												// Create customField variable to store the new value
+												let customField;
+
+												// Replace spaces with underscores
+												customField = columnHeader.replaceAll(' ', '_');
+
+												// Limit the length of the field to 40 characters
+												customField = customField.substring(0, 40);
+
+												// If the field starts with a number, add an X to the beginning of the field
+												if (customField.match(/^[0-9]/)) {
+													customField = 'X' + customField;
+												}
+
+												// Remove any special characters
+												// % & * ( ) + - = { } [ ] : ; ' " , . < > ? / | \ ~ ` ! @ # $ ^
+												customField = customField.replace(/[^a-zA-Z0-9_]/g, '');
+
+												// newlines and carriage returns are also removed
+												customField = customField.replace(/[\r\n]+/gm, '');
+
+												// Remove any leading or trailing underscores
+												customField = customField.replace(/^_+|_+$/g, '');
+
+												// Replace any double underscores with a single underscore
+												customField = customField.replace(/__+/g, '_');
+
+												// Replace any triple underscores with a single underscore
+												customField = customField.replace(/___+/g, '_');
+
+												// Add __c to the end of the field
+												customField = customField + '__c';
+
+												// Validate the field name
+												if (fieldList.includes(customField)) {
+													console.log('custom field: ' + customField);
+													newColumnHeaders.push({"newField":customField, "oldField":columnHeader});
+												} else {
+													console.log('removed field: ' + columnHeader);
+													fieldsToRemove.push(columnHeader);
+												}
+											}
+										}
+
+										console.log('newColumnHeaders: ' + JSON.stringify(newColumnHeaders));
+										console.log('fieldsToRemove: ' + JSON.stringify(fieldsToRemove));
+
+										// If fieldsToRemove is not empty then error out
+										if (fieldsToRemove.length > 0) {
+											console.log('fieldsToRemove: ' + JSON.stringify(fieldsToRemove));
+											this._errorMessage = 'The following fields are not valid: ' + fieldsToRemove.join(', ') + '. Please remove them from the CSV file and try again.';
+											this._isError = true;
+										}
+
+										// Check if there are duplicate headers
+										let duplicateHeaders = [];
+										for (let i = 0; i < newColumnHeaders.length; i++) {
+											let columnHeader = newColumnHeaders[i].newField;
+											if (newColumnHeaders.filter(x => x.newField === columnHeader).length > 1) {
+												duplicateHeaders.push(columnHeader);
+											}
+										}
+
+										// If there is a duplicate header then error out
+										if (duplicateHeaders.length > 0) {
+											console.log('duplicateHeaders: ' + JSON.stringify(duplicateHeaders));
+											this._errorMessage = 'Duplicate headers found: ' + duplicateHeaders.join(', ') + '. Please remove the duplicate headers and try again.';
+											this._isError = true;
+											this._isLoading = false;
+											return;
+										}
+
+										// New array to store the rows of data
+										let newRows = [];
+										// Go through the parsedResults.data object and set key based on the fieldList object match on oldField and replace the oldField with the newField
+										// If the key is not in the columnHeaders object, remove the key and value from the object
+										// If the key is in the fieldsToRemove object, remove the key and value from the object
+										console.log('parsedResults.length: ' + parsedResults.data.length);
+										for (let i = 0; i < parsedResults.data.length; i++) {
+											let row = parsedResults.data[i];
+											let newRow = {};
+											for (let key in row) {
+												if (row.hasOwnProperty(key)) {
+													let newKey = key;
+													let newValue = row[key];
+													for (let j = 0; j < newColumnHeaders.length; j++) {
+														if (key === newColumnHeaders[j].oldField) {
+															console.log('oldKey: ' + key + ' newKey: ' + newColumnHeaders[j].newField);
+															newKey = newColumnHeaders[j].newField;
+														}
+													}
+													if (fieldsToRemove.includes(key)) {
+														console.log('removed key: ' + key);
+														delete row[key];
+													} else {
+														console.log('newRow[' + newKey + ']: ' + newValue);
+														newRow[newKey] = newValue;
+													}
+												}
+											}
+											newRows.push(newRow);
+										}
+										
+										// Set the rows of data
+										console.log('newRows: ' + JSON.stringify(newRows));
+										this._outputValue = newRows;
+										this._isLoading = false;
+										// Set outputValue to the results
+										this.handleValueChange('outputValue', newRows);
+
+										console.log('autoNavigateNext: ' + this._autoNavigateNext);
+										// If the autoNavigateNext attribute is true, navigate to the next screen
+										if (this._autoNavigateNext) {
+											console.log('autoNavigateNext');
+											this.handleNext();
+										}
+								})
+								.catch(error => {
+										console.log('error: ' + JSON.stringify(error));
+										this._errorMessage = JSON.stringify(error);
+										this._isError = true;
+										this._isLoading = false;
+								});
+
+						},
+						error: (error) => {
+								console.error(error);
+								this._errorMessage = 'Parser Error: ' + error;
+								this._isError = true;
+								this._isLoading = false;
+						}
+				})
+		}
+	}
+
+	// Handle auto navigation to the next screen/action
+	handleNext() {
+		if (this._availableActions.find((action) => action === "NEXT")) {
+			const navigateNextEvent = new FlowNavigationNextEvent();
+			this.dispatchEvent(navigateNextEvent);
+		}
+		if (this._availableActions.find((action) => action === "FINISH")) {
+			const navigateNextEvent = new FlowNavigationFinishEvent();
+			this.dispatchEvent(navigateNextEvent);
+		}
+	}
+
+    handleValueChange(apiName, value) {
+        const attributeChangeEvent = new FlowAttributeChangeEvent(apiName, value);
+        this.dispatchEvent(attributeChangeEvent);
+    }
+}
---
+++ flow_screen_components/ConvertCSVToRecords_LWC/force-app/main/default/lwc/lwcConvertCSVToRecordsCPE/lwcConvertCSVToRecordsCPE.js
@@ -0,0 +1,144 @@
+import {LightningElement, api, track} from 'lwc';
+
+const DATA_TYPE = {
+    STRING: 'String',
+    BOOLEAN: 'Boolean',
+    NUMBER: 'Number',
+    INTEGER: 'Integer'
+};
+
+const FLOW_EVENT_TYPE = {
+    DELETE: 'configuration_editor_input_value_deleted',
+    CHANGE: 'configuration_editor_input_value_changed'
+}
+
+export default class LwcConvertCSVToRecords_CPE extends LightningElement {
+    typeValue;
+    _builderContext = {};
+    _values = [];
+    _typeMappings = [];
+
+    @track inputValues = {
+        objectName: {value: null, valueDataType: null, isCollection: false, label: 'Input Object?', required: true, errorMessage: 'Please select an object'},
+        inputLabel: {value: 'Upload CSV', valueDataType: null, isCollection: false, label: 'File Import Label', required: false, fieldHelpText: 'The label for the file input field'},
+        autoNavigateNext: {value: false, valueDataType: null, isCollection: false, label: 'Auto Navigate Next', required: true, fieldHelpText: 'If true, the flow will automatically navigate to the next screen after the component has been initialized.'},
+        delimiter: {value: ',', valueDataType: null, isCollection: false, label: 'Delimiter', required: false, fieldHelpText: 'The delimiting character. Leave blank to auto-detect from a list of most common delimiters, or any values passed in through delimitersToGuess'},
+        newLine: {value: null, valueDataType: null, isCollection: false, label: 'New Line', required: false, fieldHelpText: 'The newline sequence. Leave blank to auto-detect. Must be one of \r, \n, or \r\n.'},
+        quoteChar: {value: '&quot;', valueDataType: null, isCollection: false, label: 'Quote Character', required: false, fieldHelpText: 'The character used to quote fields. The quoting of all fields is not mandatory. Any field which is not quoted will correctly read.'},
+        escapeChar: {value: '&quot;', valueDataType: null, isCollection: false, label: 'Escape Character', required: false, fieldHelpText: 'The character used to escape the quote character within a field. If not set, this option will default to the value of quoteChar'},
+        transformHeader: {value: '', valueDataType: null, isCollection: false, label: 'Transform Header', required: false, fieldHelpText: 'A function to apply on each header.  The function receives the header as its first argument and the index as second.'},
+        dynamicTyping: {value: false, valueDataType: null, isCollection: false, label: 'Dynamic Typing', required: false, fieldHelpText: 'If true, numeric and boolean data will be converted to their type instead of remaining strings'},
+        encoding: {value: 'UTF-8', valueDataType: null, isCollection: false, label: 'Encoding', required: false, fieldHelpText: 'The encoding to use when opening local files. If specified, it must be a value supported by the FileReader API'},
+        comments: {value: false, valueDataType: null, isCollection: false, label: 'Comments', required: false, fieldHelpText: 'A string that indicates a comment (for example, # or //). When Papa encounters a line starting with this string, it will skip the line.'},
+        fastMode: {value: false, valueDataType: null, isCollection: false, label: 'Fast Mode', required: false, fieldHelpText: 'Fast mode speeds up parsing significantly for large inputs. However, it only works when the input has no quoted fields. Fast mode will automatically be enabled if no " characters appear in the input.'},
+        transform: {value: '', valueDataType: null, isCollection: false, label: 'Transform', required: false, fieldHelpText: 'A function to apply on each value. The function receives the value as its first argument and the column number or header name when enabled as its second argument. The return value of the function will replace the value it received. The transform function is applied before dynamicTyping.'},
+        delimitersToGuess: {value: '', valueDataType: null, isCollection: false, label: 'Delimiters To Guess', required: false, fieldHelpText: 'An array of delimiters to guess from if the delimiter option is not set'},
+    }
+
+    @api get builderContext() {
+        return this._builderContext;
+    }
+
+    set builderContext(value) {
+        this._builderContext = value;
+    }
+
+    @api get inputVariables() {
+        return this._values;
+    }
+
+    set inputVariables(value) {
+        this._values = value;
+        this.initializeValues();
+    }
+
+    @api get genericTypeMappings() {
+        return this._genericTypeMappings;
+    }
+    set genericTypeMappings(value) {
+        this._typeMappings = value;
+        this.initializeTypeMappings();
+    }
+
+    initializeValues(value) {
+        if (this._values && this._values.length) {
+            this._values.forEach(curInputParam => {
+                if (curInputParam.name && this.inputValues[curInputParam.name]) {
+                    console.log('in initializeValues: ' + curInputParam.name + ' = ' + curInputParam.value);
+                    // console.log('in initializeValues: '+ JSON.stringify(curInputParam));
+                    if (this.inputValues[curInputParam.name].serialized) {
+                        this.inputValues[curInputParam.name].value = JSON.parse(curInputParam.value);
+                    } else {
+                        this.inputValues[curInputParam.name].value = curInputParam.value;
+                    }
+                    this.inputValues[curInputParam.name].valueDataType = curInputParam.valueDataType;
+                }
+            });
+        }
+    }
+
+    initializeTypeMappings() {
+        this._typeMappings.forEach((typeMapping) => {
+            // console.log(JSON.stringify(typeMapping));
+            if (typeMapping.name && typeMapping.value) {
+                this.typeValue = typeMapping.value;
+            }
+        });
+    }
+
+    handleValueChange(event) {
+        console.log('in handleValueChange: ' + JSON.stringify(event));
+        if (event.detail && event.target) {
+            // Any component using fsc_flow-combobox will be ran through here
+            // This is the newer version and will allow users to use merge fields
+            // If event.detail.newValue is set then use it, otherwise use event.detail.value
+            let newValue = event.detail.newValue;
+            if (newValue == null) {
+                newValue = event.detail.value;
+
+                // If event.detail.value.name is set then use it, otherwise use event.detail.value
+                if (newValue.name != null) {
+                    newValue = newValue.name;
+                }
+            }
+            console.log('(NEW) in handleValueChange: ' + event.target.name + ' = ' + newValue);
+            this.dispatchFlowValueChangeEvent(event.target.name, newValue, event.detail.newValueDataType);
+
+        } else if ( event.detail && event.currentTarget ) {
+            // This is the older version for any old inputs that are still using currentTarget
+            // Kept for backwards compatibility
+            console.log('(OLD) in handleValueChange: ' + event.currentTarget.name + ' = ' + event.detail);
+            let dataType = DATA_TYPE.STRING;
+            if (event.currentTarget.type == 'checkbox') dataType = DATA_TYPE.BOOLEAN;
+            if (event.currentTarget.type == 'number') dataType = DATA_TYPE.NUMBER;
+            if (event.currentTarget.type == 'integer') dataType = DATA_TYPE.INTEGER;
+            let newValue = event.currentTarget.type === 'checkbox' ? event.currentTarget.checked : event.detail.value;
+            this.dispatchFlowValueChangeEvent(event.currentTarget.name, newValue, dataType);
+        } else {
+            console.log('in handleValueChange: no event detail');
+        }
+    }
+
+    handleObjectChange(event) {
+        this.dispatchFlowValueChangeEvent('objectName', event.detail.objectType, DATA_TYPE.STRING);
+    }
+
+    dispatchFlowValueChangeEvent(id, newValue, dataType = DATA_TYPE.STRING) {
+        console.log('in dispatchFlowValueChangeEvent: ' + id, newValue, dataType);
+        if (this.inputValues[id] && this.inputValues[id].serialized) {
+            console.log('serializing value');
+            newValue = JSON.stringify(newValue);
+        }
+        const valueChangedEvent = new CustomEvent(FLOW_EVENT_TYPE.CHANGE, {
+            bubbles: true,
+            cancelable: false,
+            composed: true,
+            detail: {
+                name: id,
+                newValue: newValue ? newValue : null,
+                newValueDataType: dataType
+            }
+        });
+        this.dispatchEvent(valueChangedEvent);
+    }
+}

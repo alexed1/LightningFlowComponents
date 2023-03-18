91b463065ad3d7de99af79334b7b1b0e2a5bb11b
Eric Smith <ericrsmith35@gmail.com>
2023-03-18 13:53:32 -0400

    Package Created

--- flow_apps/ImportExportFlows/packaging/CreateNewUnlockedPackage.bat
+++ flow_apps/ImportExportFlows/packaging/CreateNewUnlockedPackage.bat
@@ -1,4 +1,4 @@
 @echo off
 if "%1" neq "" set packageName=%1
 @echo on
-sfdx force:package:beta:create -v lexhost --name "%packageName%" --path force-app --packagetype Unlocked
+sfdx package:create -v lexhost --name "%packageName%" --path force-app --package-type Unlocked

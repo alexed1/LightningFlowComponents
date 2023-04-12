4d18b153dff06a6bd2672c438f804f9d3ce60197
Eric Smith <ericrsmith35@gmail.com>
2023-04-12 13:19:28 -0400

    remove colorPicker

--- flow_screen_components/FlowScreenComponentsBasePack/force-app/main/default/lwc/fsc_colorPicker/fsc_colorPicker.js
+++
@@ -1,9 +0,0 @@
-import { LightningElement, api } from 'lwc';
-
-export default class Fsc_colorPicker extends LightningElement {
-
-    handleclick(event) {
-        console.log('click',event);
-    }
-
-}

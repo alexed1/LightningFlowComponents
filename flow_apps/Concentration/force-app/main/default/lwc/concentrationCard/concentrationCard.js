de00cea270618d8186a5a08ee9b20d9997a79327
Eric Smith <ericrsmith35@gmail.com>
2023-04-17 11:54:13 -0400

    working concept

---
+++ flow_apps/Concentration/force-app/main/default/lwc/concentrationCard/concentrationCard.js
@@ -0,0 +1,106 @@
+import { LightningElement, api } from 'lwc';
+import { FlowAttributeChangeEvent } from 'lightning/flowSupport';
+import Concentration from '@salesforce/resourceUrl/fsc_Quickchoice_Images';
+
+export default class ConcentrationCard extends LightningElement {
+
+    waitValue = 1000;
+    waitEvent;
+
+    @api
+    get cardId() {
+        return this._cardId;
+    }
+    set cardId(value) {
+        this._cardId = value;
+    }
+    _cardId;
+
+    @api
+    get matchId() {
+        return this._matchId;
+    }
+    set matchId(value) {
+        console.log('Card Match: Id, match, Back, Front, Blank', this._cardId, value, this._showBack, this._showFront, this._showBlank);
+        if (!this._showBlank) {
+            console.log('Card is not Blank', this._cardId);
+            if (value == this._cardId) {
+                console.log('Card value matches', this._cardId, value);
+                this._showBack = false;
+                this._showFront = false;
+                this._showBlank = true;
+            } else {
+                console.log('Card value does not match', this._cardId, value);
+                this.waitEvent = setTimeout(() => {
+                    this._showBlank = false;
+                    this._showFront = false;
+                    this._showBack = true;
+                    this.dispatchFlowAttributeChangedEvent('exposedId', '0');
+                }, this.waitValue);
+            }
+        }
+        this._matchId = value;
+    }
+    _matchId;
+
+    @api
+    get exposedId() {
+        return this._exposedId;
+    }
+    set exposedId(value) {
+        this._exposedId = value;
+
+    }
+    _exposedId;
+
+    get showBack() {
+        return this._showBack
+    }
+    set showBack(value) {
+        this._showBack = value;
+    }
+    _showBack = true;
+
+    get showFront() {
+        return this._showFront
+    }
+    set showFront(value) {
+        this._showFront = value;
+    }
+    _showFront = false;
+
+    get showBlank() {
+        return this._showBlank;
+    }
+    set showBlank(value) {
+        this._showBlank = value;
+    }
+    _showBlank = false;
+
+    imageBack = Concentration + '/astro.png';
+    imageFront = Concentration + '/Sales.PNG';
+    imageBlank = Concentration + '/Service.PNG';
+
+    pause(milliseconds) {
+        this.waitEvent = setTimeout(() => {}, milliseconds);
+    }
+
+    clickHandler(event) {
+        console.log('Click Handler Card, showBack', this._cardId, this._showBack);
+        if (this._showBack) {
+            this._showBack = false;
+            this._showFront = true;
+            this.dispatchFlowAttributeChangedEvent('exposedId', this._cardId);
+        }
+    }
+
+    dispatchFlowAttributeChangedEvent(attributeName, attributeValue) {
+        console.log('Card Dispatch', this._cardId, attributeName, attributeValue);
+        const attributeChangeEvent = new FlowAttributeChangeEvent(
+            attributeName,
+            attributeValue
+        );
+        this.dispatchEvent(attributeChangeEvent);
+    }
+
+}
---
+++ flow_apps/Concentration/force-app/main/default/lwc/concentrationController/concentrationController.js
@@ -0,0 +1,111 @@
+import { LightningElement, api } from 'lwc';
+import { FlowAttributeChangeEvent } from 'lightning/flowSupport';
+
+export default class ConcentrationController extends LightningElement {
+
+
+    @api
+    get exposedId_11() {
+        return this._exposedId_11;
+    }
+    set exposedId_11(value) {
+        this._exposedId_11 = value;
+        this.updateExposed(value);
+    }
+    _exposedId_11;
+
+    @api
+    get exposedId_12() {
+        return this._exposedId_12;
+    }
+    set exposedId_12(value) {
+        this._exposedId_12 = value;
+        this.updateExposed(value);
+    }
+    _exposedId_12;
+
+    @api
+    get exposedId_13() {
+        return this._exposedId_13;
+    }
+    set exposedId_13(value) {
+        this._exposedId_13 = value;
+        this.updateExposed(value);
+    }
+    _exposedId_13;
+
+    @api
+    get exposedId_21() {
+        return this._exposedId_21;
+    }
+    set exposedId_21(value) {
+        this._exposedId_21 = value;
+        this.updateExposed(value);
+    }
+    _exposedId_21;
+
+    @api
+    get exposedId_22() {
+        return this._exposedId_22;
+    }
+    set exposedId_22(value) {
+        this._exposedId_22 = value;
+        this.updateExposed(value);
+    }
+    _exposedId_22;
+
+    @api
+    get exposedId_23() {
+        return this._exposedId_23;
+    }
+    set exposedId_23(value) {
+        this._exposedId_23 = value;
+        this.updateExposed(value);
+    }
+    _exposedId_23;
+
+    @api
+    get matchId() {
+        return this._matchId;
+    }
+    set matchId(value) {
+        this._matchId = value;
+    }
+    _matchId;
+
+    get isFirst() {
+        return _isFirst;
+    }
+    set isFirst(value) {
+        this._isFirst = value;
+    }
+    _isFirst = true;
+
+    card1 = '';
+    card2 = '';
+
+    updateExposed(value) {
+        console.log('CONTROLLER first, value', this._isFirst, value);
+        if (value != '0') {
+            if (this._isFirst) {
+                this.card1 = value;
+                this._isFirst = false;
+            } else {
+                this.card2 = value;
+                this._matchId = (this.card1 == this.card2) ? value : '0';
+                this.dispatchFlowAttributeChangedEvent('matchId', this._matchId);
+                this._isFirst = true;
+            }
+        }
+    }
+
+    dispatchFlowAttributeChangedEvent(attributeName, attributeValue) {
+        console.log('CONTROLLER Dispatch', attributeName, attributeValue);
+        const attributeChangeEvent = new FlowAttributeChangeEvent(
+            attributeName,
+            attributeValue
+        );
+        this.dispatchEvent(attributeChangeEvent);
+    }
+
+}

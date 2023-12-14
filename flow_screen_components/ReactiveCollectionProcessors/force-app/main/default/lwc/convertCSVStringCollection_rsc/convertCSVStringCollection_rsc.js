d7cf1286cad6093162b7213132205dcbfe469334
Eric Smith <ericrsmith35@gmail.com>
2023-12-14 15:24:17 -0500

    comments

--- flow_screen_components/ReactiveCollectionProcessors/force-app/main/default/lwc/convertCSVStringCollection_rsc/convertCSVStringCollection_rsc.js
+++ flow_screen_components/ReactiveCollectionProcessors/force-app/main/default/lwc/convertCSVStringCollection_rsc/convertCSVStringCollection_rsc.js
@@ -12,6 +12,7 @@
  * Actions:     ConvertCSVToStringCollection_CP, ConvertStringCollectionToCSV_CP
  * Original:    Convert Strings to String Collections, and Vice Versa (https://unofficialsf.com/new-flow-actions-to-convert-csv-strings-to-string-collections-and-vice-versa/)
  *              (Renamed and moved to the Collection Processors Bundle)
+ * Requires:    Collection Processors v3.2.0 or later (https://unofficialsf.com/list-actions-for-flow/)
  *       
 **/
 

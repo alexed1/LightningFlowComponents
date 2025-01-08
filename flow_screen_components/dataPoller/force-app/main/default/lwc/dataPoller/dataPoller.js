4a69e0dbd549616bf15d5d5bad402e1d5c61b685
Rebbe Pod <66928914+RebbePod@users.noreply.github.com>
2025-01-07 23:00:42 -0500

    Update dataPoller.js
    gives an option to load data initially

--- flow_screen_components/dataPoller/force-app/main/default/lwc/dataPoller/dataPoller.js
+++ flow_screen_components/dataPoller/force-app/main/default/lwc/dataPoller/dataPoller.js
@@ -1,11 +1,13 @@
 import { api, LightningElement } from 'lwc';
 import getSobjects from '@salesforce/apex/DataPollerController.getSobjects';
 import { FlowAttributeChangeEvent } from 'lightning/flowSupport';
+
 export default class DataPoller extends LightningElement {
     @api pollingFrequency = 3;
     @api queryString;
-
     @api newQueryString;
+
+    @api runInitialQueryImmediately = false;
 
     _queryId;
     @api 
@@ -14,32 +16,41 @@ export default class DataPoller extends LightningElement {
     }
     set queryId(value) {
         this._queryId = value;
-        this.newQueryString = this.queryString.replace('{Id}', value);
+        this.newQueryString = this.queryString?.replace('{Id}', value);
     }
 
     @api retrievedRecords = [];
     @api error;
 
     connectedCallback() {
+        if (!this.newQueryString && this.queryString) {
+            this.newQueryString = this.queryString;
+        }
+
         this.dispatchEvent(new FlowAttributeChangeEvent('retrievedRecords', [])); 
-        setInterval(function() {
-            getSobjects({queryString : this.newQueryString}).then(
-                result => {
-                    this.retrievedRecords = result;
-                    this.dispatchEvent(new FlowAttributeChangeEvent('retrievedRecords', this.retrievedRecords));             
-                }
-            ).catch(
-                error => {
-                    console.error('error', error);
-                    if(this.newQueryString) {
-                        this.error = error?.body?.message;
-                        this.dispatchEvent(new FlowAttributeChangeEvent('error', this.error?.body?.message)); 
-                    }
-                }
-            );
-        }.bind(this), this.pollingFrequency * 1000
-        )
-        
+
+        if (this.runInitialQueryImmediately) {
+            this.executeQuery();
+        }
+
+        setInterval(this.executeQuery.bind(this), this.pollingFrequency * 1000);
     }
 
+    executeQuery() {
+        if (!this.newQueryString) {
+            console.error('Error: queryString or newQueryString is undefined');
+            return;
+        }
+
+        getSobjects({ queryString: this.newQueryString })
+            .then(result => {
+                this.retrievedRecords = result;
+                this.dispatchEvent(new FlowAttributeChangeEvent('retrievedRecords', this.retrievedRecords));
+            })
+            .catch(error => {
+                console.error('Error executing query:', error);
+                this.error = error?.body?.message;
+                this.dispatchEvent(new FlowAttributeChangeEvent('error', this.error));
+            });
+    }
 }

017ea2dde422c510b87759b821035f712c028e43
iDamari <16044414+iDamari@users.noreply.github.com>
2025-01-28 20:59:45 +0200

    feat: extend Quick Lookup aura component with barcode reader support. ✨

--- flow_screen_components/quickLookup/force-app/main/default/aura/quickLookup/quickLookupController.js
+++ flow_screen_components/quickLookup/force-app/main/default/aura/quickLookup/quickLookupController.js
@@ -1,5 +1,24 @@
 ({
     doInit: function (component, event, helper) {
         helper.hlpCheckValidity(component, event);
+    },
+    handleScanSuccess: function (component, event, helper) {
+        component.set('v.errorMessage', '');
+        //component.set("v.filterFieldValue", scannedBarcode);
+        const componentA = component.find('QuickLightningLookup');
+        const scannedBarcode = event.getParam('value');
+        componentA.invokeLookup(scannedBarcode);
+      },
+      handleScanError: function (component, event, helper) {
+        const errorMessage = event.getParam('detail');
+        var toast = $A.get('e.force:showToast');
+        if (toast) {
+          //fire the toast event in Salesforce app and Lightning Experience
+          toast.setParams({
+            title: 'Error!',
+            message: errorMessage
+          });
+          toast.fire();
+        }
     }
 });
